8d199b53fb781630a3b93cc87a1bcacd2bf34145
Daniel Borkmann <daniel@iogearbox.net>
2017-04-17 03:12:06 +0200

    BACKPORT: bpf: fix cb access in socket filter programs on tail calls
    Commit ff936a04e5f2 ("bpf: fix cb access in socket filter programs")
    added a fix for socket filter programs such that in i) AF_PACKET the
    20 bytes of skb->cb[] area gets zeroed before use in order to not leak
    data, and ii) socket filter programs attached to TCP/UDP sockets need
    to save/restore these 20 bytes since they are also used by protocol
    layers at that time.
    
    The problem is that bpf_prog_run_save_cb() and bpf_prog_run_clear_cb()
    only look at the actual attached program to determine whether to zero
    or save/restore the skb->cb[] parts. There can be cases where the
    actual attached program does not access the skb->cb[], but the program
    tail calls into another program which does access this area. In such
    a case, the zero or save/restore is currently not performed.
    
    Since the programs we tail call into are unknown at verification time
    and can dynamically change, we need to assume that whenever the attached
    program performs a tail call, that later programs could access the
    skb->cb[], and therefore we need to always set cb_access to 1.
    
    Fixes: ff936a04e5f2 ("bpf: fix cb access in socket filter programs")
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3635,6 +3635,13 @@ static int fixup_bpf_calls(struct bpf_verifier_env *env)
 		if (insn->imm == BPF_FUNC_get_prandom_u32)
 			bpf_user_rnd_init_once();
 		if (insn->imm == BPF_FUNC_tail_call) {
+			/* If we tail call into other programs, we
+				 * cannot make any assumptions since they
+				 * can be replaced dynamically during runtime
+				 * in the program array.
+				 */
+				prog->cb_access = 1;
+
 			/* mark bpf_tail_call as different opcode to avoid
 			 * conditional branch in the interpeter for every normal
 			 * call and to prevent accidental JITing by JIT compiler
