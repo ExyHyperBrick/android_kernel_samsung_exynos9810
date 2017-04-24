b9a339c0a189535c9e572ac50c16b27c960c06e8
Daniel Borkmann <daniel@iogearbox.net>
2017-04-24 22:14:35 +0200

    BACKPORT: bpf: make bpf_xdp_adjust_head support mandatory
    Now that also the last in-tree user of the xdp_adjust_head bit has
    been removed, we can remove the flag from struct bpf_prog altogether.
    
    This, at the same time, also makes sure that any future driver for
    XDP comes with bpf_xdp_adjust_head() support right away.
    
    A rejection based on this flag would also mean that tail calls
    couldn't be used with such driver as per c2002f983767 ("bpf: fix
    checking xdp_adjust_head on tail calls") fix, thus lets not allow
    for it in the first place.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3636,12 +3636,11 @@ static int fixup_bpf_calls(struct bpf_verifier_env *env)
 			bpf_user_rnd_init_once();
 		if (insn->imm == BPF_FUNC_tail_call) {
 			/* If we tail call into other programs, we
-				 * cannot make any assumptions since they
-				 * can be replaced dynamically during runtime
-				 * in the program array.
-				 */
-				prog->cb_access = 1;
-				prog->xdp_adjust_head = 1;
+			 * cannot make any assumptions since they can
+			 * be replaced dynamically during runtime in
+			 * the program array.
+			 */
+			 prog->cb_access = 1;
 
 			/* mark bpf_tail_call as different opcode to avoid
 			 * conditional branch in the interpeter for every normal
