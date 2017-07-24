3351c55651b3ace47a7eba50db5022e1edf02ca5
Matvejchikov Ilya <matvejchikov@gmail.com>
2017-07-24 16:02:12 +0400

    UPSTREAM: tcp: remove redundant argument from tcp_rcv_established()
    The last (4th) argument of tcp_rcv_established() is redundant as it
    always equals to skb->len and the skb itself is always passed as 2th
    agrument. There is no reason to have it.
    
    Signed-off-by: Ilya V. Matveychikov <matvejchikov@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1420,7 +1420,7 @@ int tcp_v4_do_rcv(struct sock *sk, struct sk_buff *skb)
 				dst_release(dst);
 			}
 		}
-		tcp_rcv_established(sk, skb, tcp_hdr(skb), skb->len);
+		tcp_rcv_established(sk, skb, tcp_hdr(skb));
 		return 0;
 	}
 
