4056b1e087a051c3da04e377580df9d9fcf45134
Anna-Maria Gleixner <anna-maria@linutronix.de>
2017-12-21 11:41:50 +0100

    BACKPORT: hrtimer: Make remote enqueue decision less restrictive
    [ Upstream commit 2ac2dccce9d16a7b1a8fddf69a955d249375bce4 ]
    
    The current decision whether a timer can be queued on a remote CPU checks
    for timer->expiry <= remote_cpu_base.expires_next.
    
    This is too restrictive because a timer with the same expiry time as an
    existing timer will be enqueued on right-hand size of the existing timer
    inside the rbtree, i.e. behind the first expiring timer.
    
    So its safe to allow enqueuing timers with the same expiry time as the
    first expiring timer on a remote CPU base.
    
    [Mathias Gluszczynski: Apply the relaxed decision to the Samsung
     boot-cluster-aware remote enqueue path.]
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: keescook@chromium.org
    Link: http://lkml.kernel.org/r/20171221104205.7269-22-anna-maria@linutronix.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -166,7 +166,7 @@ hrtimer_check_target(struct hrtimer *timer, struct hrtimer_clock_base *new_base)
 	ktime_t expires;
 
 	expires = ktime_sub(hrtimer_get_expires(timer), new_base->offset);
-	return expires.tv64 <= new_base->cpu_base->expires_next.tv64;
+	return expires.tv64 < new_base->cpu_base->expires_next.tv64;
 }
 
 #ifdef CONFIG_NO_HZ_COMMON
