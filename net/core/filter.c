6a678d3f54e1c40f15e27c0158a32703ef9ed92e
John Fastabend <john.fastabend@gmail.com>
2018-03-19 20:14:39 +0000

    BACKPORT: bpf: sockmap, add msg_cork_bytes() helper
    [ Upstream commit 91843d540a139eb8070bcff8aa10089164436deb ]
    
    In the case where we need a specific number of bytes before a
    verdict can be assigned, even if the data spans multiple sendmsg
    or sendfile calls. The BPF program may use msg_cork_bytes().
    
    The extreme case is a user can call sendmsg repeatedly with
    1-byte msg segments. Obviously, this is bad for performance but
    is still valid. If the BPF program needs N bytes to validate
    a header it can use msg_cork_bytes to specify N bytes and the
    BPF program will not be called again until N bytes have been
    accumulated. The infrastructure will attempt to coalesce data
    if possible so in many cases (most my use cases at least) the
    data will be in a single scatterlist element with data pointers
    pointing to start/end of the element. However, this is dependent
    on available memory so is not guaranteed. So BPF programs must
    validate data pointer ranges, but this is the case anyways to
    convince the verifier the accesses are valid.
    
    [Mathias Gluszczynski: Add the helper to this tree's generic sk_msg
    implementation and register it in sk_msg_func_proto(). The helper ID
    and documentation are already present in the synchronized UAPI.]
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/filter.c
+++ net/core/filter.c
@@ -1975,6 +1975,20 @@ static const struct bpf_func_proto bpf_msg_apply_bytes_proto = {
 	.arg2_type	= ARG_ANYTHING,
 };
 
+BPF_CALL_2(bpf_msg_cork_bytes, struct sk_msg *, msg, u32, bytes)
+{
+	msg->cork_bytes = bytes;
+	return 0;
+}
+
+static const struct bpf_func_proto bpf_msg_cork_bytes_proto = {
+	.func		= bpf_msg_cork_bytes,
+	.gpl_only	= false,
+	.ret_type	= RET_INTEGER,
+	.arg1_type	= ARG_PTR_TO_CTX,
+	.arg2_type	= ARG_ANYTHING,
+};
+
 BPF_CALL_1(bpf_get_cgroup_classid, const struct sk_buff *, skb)
 {
 	return task_get_classid(skb);
@@ -4556,6 +4570,8 @@ static const struct bpf_func_proto *sk_msg_func_proto(enum bpf_func_id func_id, 
 		return &bpf_msg_redirect_hash_proto;
 	case BPF_FUNC_msg_apply_bytes:
 		return &bpf_msg_apply_bytes_proto;
+	case BPF_FUNC_msg_cork_bytes:
+		return &bpf_msg_cork_bytes_proto;
 	case BPF_FUNC_get_local_storage:
 		return &bpf_get_local_storage_proto;
 	default:
