2c77981397a4b391b76e1da390759b3c6272cd6a
Okash Khawaja <osk@fb.com>
2018-07-13 21:57:02 -0700

    BACKPORT: bpf: btf: Export types and names by offset
    [ Upstream commit 92b57121ca79b286bef4f304e887272f3f2d86bb ]
    
    This patch introduces btf__resolve_type() function and exports two
    existing functions from libbpf. btf__resolve_type follows modifier
    types like const and typedef until it hits a type which actually takes
    up memory, and then returns it. This function follows similar pattern
    to btf__resolve_size but instead of computing size, it just returns
    the type.
    
    These  functions will be used in the followig patch which parses
    information inside array of `struct btf_type *`. btf_name_by_offset is
    used for printing variable names.
    
    [Mathias Gluszczynski: Export the two accessors used by
    resolve_btfids. The deliberately read-only parser does not carry the
    unneeded type-size resolution API.]
    
    Signed-off-by: Okash Khawaja <osk@fb.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- tools/lib/bpf/btf.h
+++ tools/lib/bpf/btf.h
@@ -13,5 +13,7 @@ struct btf;
 
 void btf__free(struct btf *btf);
 struct btf *btf__new(const void *data, __u32 size);
+const char *btf__name_by_offset(const struct btf *btf, __u32 offset);
+const struct btf_type *btf__type_by_id(const struct btf *btf, __u32 id);
 
 #endif /* __LIBBPF_BTF_H */
