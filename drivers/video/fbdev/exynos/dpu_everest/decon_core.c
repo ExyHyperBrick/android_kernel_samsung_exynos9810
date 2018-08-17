21f12d625d68850ec8b3131e0fe679509100684c
Jaehoe Yang <jaehoe.yang@samsung.com>
2018-08-17 16:35:06 +0900

    fbdev: dpu_everest: add color mode ioctl.
    Followings are added ioctl
    - EXYNOS_GET_COLOR_MODE_NUM
    - EXYNOS_GET_COLOR_MODE
    - EXYNOS_SET_COLOR_MODE
    
    Change-Id: Id44c882dcf4a16262a4a8090e607fa7edc5c7906
    Signed-off-by: Jaehoe Yang <jaehoe.yang@samsung.com>

--- drivers/video/fbdev/exynos/dpu_everest/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_everest/decon_core.c
@@ -2705,6 +2705,62 @@ static int decon_get_hdr_capa_info(struct decon_device *decon,
 
 }
 
+static int decon_get_color_mode(struct decon_device *decon,
+		struct decon_color_mode_info *color_mode)
+{
+	int ret = 0;
+
+	decon_dbg("%s +\n", __func__);
+	mutex_lock(&decon->lock);
+
+	switch (color_mode->index) {
+	case 0:
+		color_mode->color_mode = HAL_COLOR_MODE_NATIVE;
+		break;
+
+	/* TODO: add supporting color mode if necessary */
+
+	default:
+		decon_err("%s: queried color mode index is wrong!(%d)\n",
+			__func__, color_mode->index);
+		ret = -EINVAL;
+		break;
+	}
+
+	mutex_unlock(&decon->lock);
+	decon_dbg("%s -\n", __func__);
+
+	return ret;
+}
+
+static int decon_set_color_mode(struct decon_device *decon,
+		struct decon_color_mode_info *color_mode)
+{
+	int ret = 0;
+
+	decon_dbg("%s +\n", __func__);
+	mutex_lock(&decon->lock);
+
+	switch (color_mode->index) {
+	case 0:
+		color_mode->color_mode = HAL_COLOR_MODE_NATIVE;
+		break;
+
+	/* TODO: add supporting color mode if necessary */
+
+	default:
+		decon_err("%s: color mode index is out of range!(%d)\n",
+			__func__, color_mode->index);
+		ret = -EINVAL;
+		break;
+	}
+
+	mutex_unlock(&decon->lock);
+	decon_dbg("%s -\n", __func__);
+
+	return ret;
+}
+
 static int decon_ioctl(struct fb_info *info, unsigned int cmd,
 			unsigned long arg)
 {
@@ -2722,6 +2778,7 @@ static int decon_ioctl(struct fb_info *info, unsigned int cmd,
 	struct decon_disp_info __user *argp_info;
 	struct decon_display_mode dm_info;
  	struct decon_reg_data decon_regs;
+	struct decon_color_mode_info cm_info;
 	int ret = 0;
 	u32 crtc;
 	bool active;
@@ -2729,6 +2786,7 @@ static int decon_ioctl(struct fb_info *info, unsigned int cmd,
 	u32 crc_data[2];
 	u32 pwr_mode;
 	u32 dm_num;
+	u32 cm_num;
 
 	decon_hiber_block_exit(decon);
 	switch (cmd) {
@@ -3036,6 +3094,46 @@ static int decon_ioctl(struct fb_info *info, unsigned int cmd,
 			ret = -EINVAL;
 			break;
 		}
+
+		break;
+
+	case EXYNOS_GET_COLOR_MODE_NUM:
+		cm_num = HAL_COLOR_MODE_NUM_MAX;
+		if (copy_to_user((u32 __user *)arg, &cm_num, sizeof(u32)))
+			ret = -EFAULT;
+		break;
+
+	case EXYNOS_GET_COLOR_MODE:
+		if (copy_from_user(&cm_info,
+				   (struct decon_color_mode_info __user *)arg,
+				   sizeof(struct decon_color_mode_info))) {
+			ret = -EFAULT;
+			break;
+		}
+
+		ret = decon_get_color_mode(decon, &cm_info);
+		if (ret)
+			break;
+
+		if (copy_to_user((struct decon_color_mode_info __user *)arg,
+				&cm_info,
+				sizeof(struct decon_color_mode_info))) {
+			ret = -EFAULT;
+			break;
+		}
+		break;
+
+	case EXYNOS_SET_COLOR_MODE:
+		if (get_user(cm_info.index, (int __user *)arg)) {
+			ret = -EFAULT;
+			break;
+		}
+
+		ret = decon_set_color_mode(decon, &cm_info);
+		if (ret)
+			break;
+
+		/* ADD additional action if necessary */
 
 		break;
 
