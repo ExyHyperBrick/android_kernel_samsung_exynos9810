753e5fcb001f7e821d3d0182a45f94b90b90b851
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2018-11-23 23:18:03 +0100

    BACKPORT: bpf: add __weak hook for allocating executable memory
    [ Upstream commit dc002bb62f10c5905420f8b8a7d5ec0da567fc82 ]
    
    By default, BPF uses module_alloc() to allocate executable memory,
    but this is not necessary on all arches and potentially undesirable
    on some of them.
    
    So break out the module_alloc() and module_memfree() calls into __weak
    functions to allow them to be overridden in arch code.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -533,6 +533,16 @@ static void bpf_jit_uncharge_modmem(u32 pages)
 	atomic_long_sub(pages, &bpf_jit_current);
 }
 
+void *__weak bpf_jit_alloc_exec(unsigned long size)
+{
+	return module_alloc(size);
+}
+
+void __weak bpf_jit_free_exec(void *addr)
+{
+	module_memfree(addr);
+}
+
 static __always_inline void
 bpf_get_prog_addr_region(const struct bpf_prog *prog,
 			 unsigned long *symbol_start,
@@ -791,7 +801,7 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 
 	if (bpf_jit_charge_modmem(pages))
 		return NULL;
-	hdr = module_alloc(size);
+	hdr = bpf_jit_alloc_exec(size);
 	if (!hdr) {
 		bpf_jit_uncharge_modmem(pages);
 		return NULL;
@@ -815,7 +825,7 @@ void bpf_jit_binary_free(struct bpf_binary_header *hdr)
 {
 	u32 pages = hdr->pages;
 
-	module_memfree(hdr);
+	bpf_jit_free_exec(hdr);
 	bpf_jit_uncharge_modmem(pages);
 }
 
