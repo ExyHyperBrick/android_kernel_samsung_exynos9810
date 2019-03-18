6b55f3e654e4105cf44df9f28397a7eb283d683c
Martin KaFai Lau <kafai@fb.com>
2019-03-18 10:37:13 -0700

    BACKPORT: bpf: Print ref_obj_id only for reference types
    [ Upstream commit cba368c1f01c27ed62fca7a853531845d263bb01 ]
    
    Naresh reported that test_align fails because of a mismatch in the
    verbose register-state output after adding ref_obj_id.
    
    ref_obj_id is useful only for registers which may carry a reference.
    Print it only for those register types and use a comma to separate it
    from id.
    
    [Mathias Gluszczynski: Include the later PTR_TO_SOCK_COMMON and
    ring-buffer PTR_TO_MEM families already present in this verifier.]
    
    Fixes: 1b986589680a ("bpf: Fix bpf_tcp_sock and bpf_sk_fullsock
    issue related to bpf_sk_release")
    Reported-by: Naresh Kamboju <naresh.kamboju@linaro.org>
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -391,6 +391,18 @@ static bool reg_may_point_to_spin_lock(const struct bpf_reg_state *reg)
 		map_value_has_spin_lock(reg->map_ptr);
 }
 
+static bool reg_type_may_be_refcounted_or_null(enum bpf_reg_type type)
+{
+	return type == PTR_TO_SOCKET ||
+	       type == PTR_TO_SOCKET_OR_NULL ||
+	       type == PTR_TO_SOCK_COMMON ||
+	       type == PTR_TO_SOCK_COMMON_OR_NULL ||
+	       type == PTR_TO_TCP_SOCK ||
+	       type == PTR_TO_TCP_SOCK_OR_NULL ||
+	       type == PTR_TO_MEM ||
+	       type == PTR_TO_MEM_OR_NULL;
+}
+
 static bool arg_type_may_be_refcounted(enum bpf_arg_type type)
 {
 	return type == ARG_PTR_TO_SOCK_COMMON ||
@@ -501,8 +513,9 @@ static void print_verifier_state(struct bpf_verifier_env *env,
 		} else {
 			if (t == PTR_TO_BTF_ID)
 				verbose(env, "%s", kernel_type_name(reg->btf_id));
-			verbose(env, "(id=%d ref_obj_id=%d", reg->id,
-				reg->ref_obj_id);
+			verbose(env, "(id=%d", reg->id);
+			if (reg_type_may_be_refcounted_or_null(t))
+				verbose(env, ",ref_obj_id=%d", reg->ref_obj_id);
 			if (t != SCALAR_VALUE)
 				verbose(env, ",off=%d", reg->off);
 			if (type_is_pkt_pointer(t))
