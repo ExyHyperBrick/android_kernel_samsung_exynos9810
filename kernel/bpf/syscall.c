7415dd5cdeb0f7dd0cf0cc4f7203cd499bb2c0f5
Stanislav Fomichev <sdf@google.com>
2019-04-11 08:21:40 +0000

    BACKPORT: bpf: support input __sk_buff context in BPF_PROG_TEST_RUN
    [ Upstream commit b0b9395d865e3060d97658fbc9ba3f77fecc8da1 ]
    
    Add new set of arguments to bpf_attr for BPF_PROG_TEST_RUN:
    * ctx_in/ctx_size_in - input context
    * ctx_out/ctx_size_out - output context
    
    The intended use case is to pass some meta data to the test runs that
    operate on skb (this has being brought up on recent LPC).
    
    For programs that use bpf_prog_test_run_skb, support __sk_buff input and
    output. Initially, from input __sk_buff, copy _only_ cb and priority into
    skb, all other non-zero fields are prohibited (with EINVAL).
    If the user has set ctx_out/ctx_size_out, copy the potentially modified
    __sk_buff back to the userspace.
    
    We require all fields of input __sk_buff except the ones we explicitly
    support to be set to zero. The expectation is that in the future we might
    add support for more fields and we want to fail explicitly if the user
    runs the program on the kernel where we don't yet support them.
    
    The API is intentionally vague (i.e. we don't explicitly add __sk_buff
    to bpf_attr, but ctx_in) to potentially let other test_run types use
    this interface in the future (this can be xdp_md for xdp types for
    example).
    
    v4:
      * don't copy more than allowed in bpf_ctx_init [Martin]
    
    v3:
      * handle case where ctx_in is NULL, but ctx_out is not [Martin]
      * convert size==0 checks to ptr==NULL checks and add some extra ptr
        checks [Martin]
    
    v2:
      * Addressed comments from Martin Lau
    
    [Mathias Gluszczynski: Adapt context I/O to the Linux 4.9 runner.]
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2840,7 +2840,7 @@ static int bpf_prog_query(const union bpf_attr *attr,
 }
 #endif /* CONFIG_CGROUP_BPF */
 
-#define BPF_PROG_TEST_RUN_LAST_FIELD test.duration
+#define BPF_PROG_TEST_RUN_LAST_FIELD test.ctx_out
 
 static int bpf_prog_test_run(const union bpf_attr *attr,
 			     union bpf_attr __user *uattr)
@@ -2851,6 +2851,14 @@ static int bpf_prog_test_run(const union bpf_attr *attr,
 	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
 	if (CHECK_ATTR(BPF_PROG_TEST_RUN))
+		return -EINVAL;
+
+	if ((attr->test.ctx_size_in && !attr->test.ctx_in) ||
+	    (!attr->test.ctx_size_in && attr->test.ctx_in))
+		return -EINVAL;
+
+	if ((attr->test.ctx_size_out && !attr->test.ctx_out) ||
+	    (!attr->test.ctx_size_out && attr->test.ctx_out))
 		return -EINVAL;
 
 	prog = bpf_prog_get(attr->test.prog_fd);
