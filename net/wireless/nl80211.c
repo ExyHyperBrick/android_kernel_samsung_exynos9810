7d5864d2cea07ebec08836f7e57e2f88271df847
Chung-Hsien Hsu <stanley.hsu@cypress.com>
2019-05-09 09:49:05 +0000

    nl80211: add WPA3 definition for SAE authentication
    Add definition of WPA version 3 for SAE authentication.
    
    Change-Id: I19ca34b8965168f011cc1352eba420f2d54b0258
    Signed-off-by: Chung-Hsien Hsu <stanley.hsu@cypress.com>
    Signed-off-by: Chi-Hsien Lin <chi-hsien.lin@cypress.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: krazey <admin@krazey.de>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -7766,6 +7766,7 @@ static bool nl80211_valid_wpa_versions(u32 wpa_versions)
 {
 	return !(wpa_versions & ~(NL80211_WPA_VERSION_1 |
 				  NL80211_WPA_VERSION_2 |
+				  NL80211_WPA_VERSION_3 |
 /*WAPI*/
 				  NL80211_WAPI_VERSION_1));
 }
