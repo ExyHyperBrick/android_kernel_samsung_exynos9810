e52ce7363e3097bf8e190e49edbedd89c5286bc8
Alexei Starovoitov <ast@kernel.org>
2019-11-14 10:57:03 -0800

    BACKPORT: bpf: Add bpf_arch_text_poke() helper
    [ Upstream commit 5964b2000f283ff5df366f718e0f083ebbaae977 ]
    
    Add bpf_arch_text_poke() helper that is used by BPF trampoline logic to
    patch
    nops/calls in kernel text into calls into BPF trampoline and to patch
    calls/nops inside BPF programs too.
    
    [Mathias Gluszczynski: Keep the generic API and weak -ENOTSUPP
    fallback. Omit the upstream x86 JIT refactor and text-poke backend
    because this Exynos9810 target is arm64, where upstream Linux 5.15 also
    has no text-poke or trampoline code generator.]
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20191114185720.1641606-4-ast@kernel.org
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -2388,6 +2388,13 @@ int __weak skb_copy_bits(const struct sk_buff *skb, int offset, void *to,
 	return -EFAULT;
 }
 
+int __weak
+bpf_arch_text_poke(void *ip, enum bpf_text_poke_type t,
+		   void *old_addr, void *new_addr)
+{
+	return -ENOTSUPP;
+}
+
 /* All definitions of tracepoints related to BPF. */
 #define CREATE_TRACE_POINTS
 #include <linux/bpf_trace.h>
