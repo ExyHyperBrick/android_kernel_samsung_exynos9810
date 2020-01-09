a3822ddf5a6a37dfe543b5792e76c332b2229779
Martin KaFai Lau <kafai@fb.com>
2020-01-09 08:46:18 -0800

    BACKPORT: bpf: Avoid storing modifier in info->btf_id
    [ Upstream commit 275517ff452a535da5eef25b1c22e53fc50b0a12 ]
    
    info->btf_id expects the BTF ID of a structure, so store the final ID
    after skipping any modifiers.
    
    Also add a missing newline to one of the BTF verifier log messages.
    
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20200109003456.3855176-1-kafai@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -3376,12 +3376,14 @@ bool btf_ctx_access(int off, int size, enum bpf_access_type type,
 
 	/* this is a pointer to another type */
 	info->reg_type = PTR_TO_BTF_ID;
-	info->btf_id = t->type;
 
+	info->btf_id = t->type;
 	t = btf_type_by_id(btf_vmlinux, t->type);
 	/* skip modifiers */
-	while (btf_type_is_modifier(t))
+	while (btf_type_is_modifier(t)) {
+		info->btf_id = t->type;
 		t = btf_type_by_id(btf_vmlinux, t->type);
+	}
 	if (!btf_type_is_struct(t)) {
 		bpf_log(log,
 			"func '%s' arg%d type %s is not a struct\n",
@@ -3407,7 +3409,7 @@ int btf_struct_access(struct bpf_verifier_log *log,
 again:
 	tname = __btf_name_by_offset(btf_vmlinux, t->name_off);
 	if (!btf_type_is_struct(t)) {
-		bpf_log(log, "Type '%s' is not a struct", tname);
+		bpf_log(log, "Type '%s' is not a struct\n", tname);
 		return -EINVAL;
 	}
 
