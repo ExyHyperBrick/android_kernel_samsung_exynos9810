c1fbbb029e90a13f5a8219d3ca578a2bb34b49aa
Qais Yousef <qais.yousef@arm.com>
2020-01-14 21:09:47 +0000

    BACKPORT: sched/uclamp: Reject negative values in cpu_uclamp_write()
    The check to ensure that the new written value into cpu.uclamp.{min,max}
    is within range, [0:100], wasn't working because of the signed
    comparison
    
     7301                 if (req.percent > UCLAMP_PERCENT_SCALE) {
     7302                         req.ret = -ERANGE;
     7303                         return req;
     7304                 }
    
    	# echo -1 > cpu.uclamp.min
    	# cat cpu.uclamp.min
    	42949671.96
    
    Cast req.percent into u64 to force the comparison to be unsigned and
    work as intended in capacity_from_percent().
    
    	# echo -1 > cpu.uclamp.min
    	sh: write error: Numerical result out of range
    
    Bug: 120440300
    Fixes: 2480c093130f ("sched/uclamp: Extend CPU's cgroup controller")
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200114210947.14083-1-qais.yousef@arm.com
    (cherry picked from commit b562d140649966d4daedd0483a8fe59ad3bb465a)
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: I17fc2b119dcbffb212e130ed2c37ae3a8d5bbb61
    
    (cherry picked from commit cb0a2cdd480b4c5a1b854c01258c976fe1fd1ebb)
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9519,7 +9519,7 @@ capacity_from_percent(char *buf)
 					     &req.percent);
 		if (req.ret)
 			return req;
-		if (req.percent > UCLAMP_PERCENT_SCALE) {
+		if ((u64)req.percent > UCLAMP_PERCENT_SCALE) {
 			req.ret = -ERANGE;
 			return req;
 		}
