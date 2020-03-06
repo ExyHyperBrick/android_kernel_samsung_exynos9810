5b7fab6c9227e10561bb63cb5eeb88bc44f9613d
Eelco Chaudron <echaudro@redhat.com>
2020-03-06 08:59:23 +0000

    BACKPORT: bpf: Add bpf_xdp_output() helper
    [ Upstream commit d831ee84bfc9173eecf30dbbc2553ae81b996c60 ]
    
    Introduce new helper that reuses existing xdp perf_event output
    implementation, but can be called from raw_tracepoint programs
    that receive 'struct xdp_buff *' as a tracepoint argument.
    
    [Mathias Gluszczynski: Reused the carried ID and current BTF-ID fields
    in the tracing dispatcher.]
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://lore.kernel.org/bpf/158348514556.2239.11050972434793741444.stgit@xdp-tutorial
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -1164,6 +1164,7 @@ static const struct bpf_func_proto bpf_perf_event_output_proto_raw_tp = {
 };
 
 extern const struct bpf_func_proto bpf_skb_output_proto;
+extern const struct bpf_func_proto bpf_xdp_output_proto;
 
 BPF_CALL_3(bpf_get_stackid_raw_tp, struct bpf_raw_tracepoint_args *, args,
 	struct bpf_map *, map, u64, flags)
@@ -1240,6 +1241,8 @@ tracing_prog_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 #ifdef CONFIG_NET
 	case BPF_FUNC_skb_output:
 		return &bpf_skb_output_proto;
+	case BPF_FUNC_xdp_output:
+		return &bpf_xdp_output_proto;
 	case BPF_FUNC_skc_to_tcp6_sock:
 		return &bpf_skc_to_tcp6_sock_proto;
 	case BPF_FUNC_skc_to_tcp_sock:
