99bbb9a8eef70417e4a28fdbae8a4ef2ed31a650
Yonghong Song <yhs@fb.com>
2020-06-23 16:08:11 -0700

    BACKPORT: bpf: Add more bpf_skc_to_*() TCP helpers
    [ Upstream commit 478cfbdf5f13dfe09cfd0b1cbac821f5e27f6108 ]
    
    Three more helpers are added to cast a sock_common pointer to
    an tcp_sock, tcp_timewait_sock or a tcp_request_sock for
    tracing programs.
    
    [Mathias Gluszczynski: Use the per-argument BTF-ID interface available in
    this tree and reuse the socket type table introduced by the
    preceding helper. Guard the IPv4 and built-in IPv6 protocol objects so
    the older configuration matrix remains buildable. The Android UAPI
    already contains all three helper numbers.]
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200623230811.3988277-1-yhs@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1043,6 +1043,9 @@ extern const struct bpf_func_proto bpf_ringbuf_discard_proto;
 extern const struct bpf_func_proto bpf_ringbuf_query_proto;
 extern const struct bpf_func_proto bpf_for_each_map_elem_proto;
 extern const struct bpf_func_proto bpf_skc_to_tcp6_sock_proto;
+extern const struct bpf_func_proto bpf_skc_to_tcp_sock_proto;
+extern const struct bpf_func_proto bpf_skc_to_tcp_timewait_sock_proto;
+extern const struct bpf_func_proto bpf_skc_to_tcp_request_sock_proto;
 /* Shared helpers among cBPF and eBPF. */
 void bpf_user_rnd_init_once(void);
 u64 bpf_user_rnd_u32(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5);
