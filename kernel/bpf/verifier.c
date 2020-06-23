fcb87ab3c51ed36dcdfab02062ccdc29a02e87b2
Yonghong Song <yhs@fb.com>
2020-06-23 16:08:11 -0700

    BACKPORT: bpf: Add more bpf_skc_to_*() TCP helpers
    [ Upstream commit 478cfbdf5f13dfe09cfd0b1cbac821f5e27f6108 ]
    
    Three more helpers are added to cast a sock_common pointer to
    an tcp_sock, tcp_timewait_sock or a tcp_request_sock for
    tracing programs.
    
    [Mathias Gluszczynski: Use the per-argument BTF-ID interface from this
    recovery branch and reuse the socket type table introduced by the
    preceding helper. Guard the IPv4 and built-in IPv6 protocol objects so
    the older configuration matrix remains buildable. The Android UAPI
    already contains all three helper numbers.]
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200623230811.3988277-1-yhs@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -440,7 +440,10 @@ static bool is_ptr_cast_function(enum bpf_func_id func_id)
 {
 	return func_id == BPF_FUNC_tcp_sock ||
 		func_id == BPF_FUNC_sk_fullsock ||
-		func_id == BPF_FUNC_skc_to_tcp6_sock;
+		func_id == BPF_FUNC_skc_to_tcp6_sock ||
+		func_id == BPF_FUNC_skc_to_tcp_sock ||
+		func_id == BPF_FUNC_skc_to_tcp_timewait_sock ||
+		func_id == BPF_FUNC_skc_to_tcp_request_sock;
 }
 
 /* string representation of 'enum bpf_reg_type' */
