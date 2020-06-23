c34c4d87121b03f4d9257c6660631df4f72503ef
Yonghong Song <yhs@fb.com>
2020-06-23 16:08:15 -0700

    BACKPORT: bpf: Add bpf_skc_to_udp6_sock() helper
    [ Upstream commit 0d4fad3e57df2bf61e8ffc8d12a34b1caf9b8835 ]
    
    The helper is used in tracing programs to cast a socket
    pointer to a udp6_sock pointer.
    The return value could be NULL if the casting is illegal.
    
    [Mathias Gluszczynski: Adapt the prototype to the existing per-argument
    BTF-ID interface and reuse the socket type table. The Android UAPI
    already contains this helper number.]
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Link: https://lore.kernel.org/bpf/20200623230815.3988481-1-yhs@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -443,7 +443,8 @@ static bool is_ptr_cast_function(enum bpf_func_id func_id)
 		func_id == BPF_FUNC_skc_to_tcp6_sock ||
 		func_id == BPF_FUNC_skc_to_tcp_sock ||
 		func_id == BPF_FUNC_skc_to_tcp_timewait_sock ||
-		func_id == BPF_FUNC_skc_to_tcp_request_sock;
+		func_id == BPF_FUNC_skc_to_tcp_request_sock ||
+		func_id == BPF_FUNC_skc_to_udp6_sock;
 }
 
 /* string representation of 'enum bpf_reg_type' */
