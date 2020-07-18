ab8ca0ca13c24ce9c324d1749945baa6267c12de
Jakub Sitnicki <jakub@cloudflare.com>
2020-07-17 20:18:17 -0700

    BACKPORT: inet6: Extract helper for selecting reuseport socket
    [ Upstream commit 5df6531292b5021ac9e4ed261eb7d1fa9ff3bf08 ]
    
    Prepare for calling into reuseport from inet6_lookup_listener as well.
    
    [Mathias Gluszczynski: Extract the helper from Linux 4.9's single
    listening-hash lookup and retain its hash for tie breaking.]
    
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200717103536.397595-6-jakub@cloudflare.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/ipv6/inet6_hashtables.c
+++ net/ipv6/inet6_hashtables.c
@@ -125,6 +125,25 @@ static inline int compute_score(struct sock *sk, struct net *net,
 	return score;
 }
 
+static struct sock *lookup_reuseport(struct net *net, struct sock *sk,
+				     struct sk_buff *skb, int doff,
+				     const struct in6_addr *saddr,
+				     __be16 sport,
+				     const struct in6_addr *daddr,
+				     unsigned short hnum, u32 *phash)
+{
+	struct sock *reuse_sk = NULL;
+	u32 hash;
+
+	if (sk->sk_reuseport) {
+		hash = inet6_ehashfn(net, daddr, hnum, saddr, sport);
+		if (phash)
+			*phash = hash;
+		reuse_sk = reuseport_select_sock(sk, hash, skb, doff);
+	}
+	return reuse_sk;
+}
+
 /* called with rcu_read_lock() */
 struct sock *inet6_lookup_listener(struct net *net,
 		struct inet_hashinfo *hashinfo,
@@ -146,10 +165,9 @@ struct sock *inet6_lookup_listener(struct net *net,
 		if (score > hiscore) {
 			reuseport = sk->sk_reuseport;
 			if (reuseport) {
-				phash = inet6_ehashfn(net, daddr, hnum,
-						      saddr, sport);
-				result = reuseport_select_sock(sk, phash,
-							       skb, doff);
+				result = lookup_reuseport(net, sk, skb, doff,
+						  saddr, sport, daddr, hnum,
+						  &phash);
 				if (unlikely(IS_ERR(result)))
 					return NULL;
 				if (result)
