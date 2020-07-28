23c23e81af10ab065a14e6be7215adc5b47a094b
Jean-Philippe Brucker <jean-philippe@linaro.org>
2020-07-28 17:21:26 +0200

    BACKPORT: bpf: arm64: Add BPF exception tables
    [ Upstream commit 800834285361dcf8e98b018e891df876472a4fac ]
    
    When a tracing BPF program attempts to read memory without using the
    bpf_probe_read() helper, the verifier marks the load instruction with
    the BPF_PROBE_MEM flag. Since the arm64 JIT does not currently recognize
    this flag it falls back to the interpreter.
    
    Add support for BPF_PROBE_MEM, by appending an exception table to the
    BPF program. If the load instruction causes a data abort, the fixup
    infrastructure finds the exception table and fixes up the fault, by
    clearing the destination register and jumping over the faulting
    instruction.
    
    To keep the compact exception table entry format, inspect the pc in
    fixup_exception(). A more generic solution would add a "handler" field
    to the table entry, like on x86 and s390.
    
    [Mathias Gluszczynski: Declare the fixup hook in asm/uaccess.h, where
    this 4.9 tree still defines struct exception_table_entry. Pass the
    legacy search_extable() API an inclusive final entry instead of Linux
    5.15's entry count.]
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200728152122.1292756-2-jean-philippe@linaro.org
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- arch/arm64/include/asm/uaccess.h
+++ arch/arm64/include/asm/uaccess.h
@@ -63,6 +63,18 @@ struct exception_table_entry
 
 #define ARCH_HAS_RELATIVE_EXTABLE
 
+#ifdef CONFIG_BPF_JIT
+int arm64_bpf_fixup_exception(const struct exception_table_entry *ex,
+			      struct pt_regs *regs);
+#else /* !CONFIG_BPF_JIT */
+static inline
+int arm64_bpf_fixup_exception(const struct exception_table_entry *ex,
+			      struct pt_regs *regs)
+{
+	return 0;
+}
+#endif /* !CONFIG_BPF_JIT */
+
 extern int fixup_exception(struct pt_regs *regs);
 
 #define get_ds()	(KERNEL_DS)
