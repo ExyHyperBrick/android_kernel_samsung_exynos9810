aec384838186815505c4ae51ef0d78f351794368
Yonghong Song <yhs@fb.com>
2020-08-21 11:44:18 -0700

    BACKPORT: bpf: Implement link_query for bpf iterators
    [ Upstream commit 6b0a249a301e2af9adda84adbced3a2988248b95 ]
    
    This patch implemented bpf_link callback functions
    show_fdinfo and fill_link_info to support link_query
    interface.
    
    The general interface for show_fdinfo and fill_link_info
    will print/fill the target_name. Each targets can
    register show_fdinfo and fill_link_info callbacks
    to print/fill more target specific information.
    
    For example, the below is a fdinfo result for a bpf
    task iterator.
      $ cat /proc/1749/fdinfo/7
      pos:    0
      flags:  02000000
      mnt_id: 14
      link_type:      iter
      link_id:        11
      prog_tag:       990e1f8152f7e54f
      prog_id:        59
      target_name:    task
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200821184418.574122-1-yhs@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -920,6 +920,10 @@ typedef int (*bpf_iter_attach_target_t)(struct bpf_prog *prog,
 					union bpf_iter_link_info *linfo,
 					struct bpf_iter_aux_info *aux);
 typedef void (*bpf_iter_detach_target_t)(struct bpf_iter_aux_info *aux);
+typedef void (*bpf_iter_show_fdinfo_t)(const struct bpf_iter_aux_info *aux,
+				       struct seq_file *seq);
+typedef int (*bpf_iter_fill_link_info_t)(const struct bpf_iter_aux_info *aux,
+					 struct bpf_link_info *info);
 
 enum bpf_iter_feature {
 	BPF_ITER_RESCHED	= BIT(0),
@@ -930,6 +934,8 @@ struct bpf_iter_reg {
 	const char *target;
 	bpf_iter_attach_target_t attach_target;
 	bpf_iter_detach_target_t detach_target;
+	bpf_iter_show_fdinfo_t show_fdinfo;
+	bpf_iter_fill_link_info_t fill_link_info;
 	u32 ctx_arg_info_size;
 	u32 feature;
 	struct bpf_ctx_arg_aux ctx_arg_info[BPF_ITER_CTX_ARG_MAX];
