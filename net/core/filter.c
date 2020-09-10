ad0bbaa8f5b20e1a1377f60a22fb2a7548176294
Neal Cardwell <ncardwell@google.com>
2020-09-10 15:35:35 -0400

    BACKPORT: tcp: Remove unused flags from BPF setsockopt
    [ Upstream commit 5cdc744caab7cb0d0a3ed479246cbab3dbc59c0e ]
    
    Now that the previous patches have removed the code that uses the
    flags argument to _bpf_setsockopt(), we can remove that argument.
    
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Acked-by: Kevin Yang <yyd@google.com>
    Cc: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3714,7 +3714,7 @@ static const struct bpf_func_proto bpf_get_socket_uid_proto = {
 };
 
 static int _bpf_setsockopt(struct sock *sk, int level, int optname,
-			   char *optval, int optlen, u32 flags)
+			   char *optval, int optlen)
 {
 	int ret = 0;
 	int val;
@@ -3855,7 +3855,7 @@ err_clear:
 BPF_CALL_5(bpf_sock_addr_setsockopt, struct bpf_sock_addr_kern *, ctx,
 	   int, level, int, optname, char *, optval, int, optlen)
 {
-	return _bpf_setsockopt(ctx->sk, level, optname, optval, optlen, 0);
+	return _bpf_setsockopt(ctx->sk, level, optname, optval, optlen);
 }
 
 static const struct bpf_func_proto bpf_sock_addr_setsockopt_proto = {
@@ -3889,10 +3889,7 @@ static const struct bpf_func_proto bpf_sock_addr_getsockopt_proto = {
 BPF_CALL_5(bpf_sock_ops_setsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 	   int, level, int, optname, char *, optval, int, optlen)
 {
-	u32 flags = 0;
-
-	return _bpf_setsockopt(bpf_sock->sk, level, optname, optval, optlen,
-			       flags);
+	return _bpf_setsockopt(bpf_sock->sk, level, optname, optval, optlen);
 }
 
 static const struct bpf_func_proto bpf_sock_ops_setsockopt_proto = {
