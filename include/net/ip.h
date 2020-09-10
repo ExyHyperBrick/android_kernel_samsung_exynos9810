4b31bac2addbf15ca264b981324e28ccfbc57cf2
Wei Wang <weiwan@google.com>
2020-09-09 17:50:47 -0700

    BACKPORT: ip: pass tos into ip_build_and_send_pkt()
    [ Upstream commit de033b7d1568a8f1252055c96cdd99954d5450c4 ]
    
    This commit adds tos as a new passed in parameter to
    ip_build_and_send_pkt() which will be used in the later commit.
    This is a pure restructure and does not have any functional change.
    
    [Mathias Gluszczynski: Adapted the IPv4 packet builder and its TCP and
    DCCP callers in the Exynos9810 4.9 tree.]
    
    Signed-off-by: Wei Wang <weiwan@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- include/net/ip.h
+++ include/net/ip.h
@@ -122,7 +122,7 @@ int igmp_mc_init(void);
 
 int ip_build_and_send_pkt(struct sk_buff *skb, const struct sock *sk,
 			  __be32 saddr, __be32 daddr,
-			  struct ip_options_rcu *opt);
+			  struct ip_options_rcu *opt, u8 tos);
 int ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt,
 	   struct net_device *orig_dev);
 int ip_local_deliver(struct sk_buff *skb);
