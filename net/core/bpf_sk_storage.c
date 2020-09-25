5cbc22ba11224256b894fab9314a45eca34beb62
Martin KaFai Lau <kafai@fb.com>
2020-09-25 20:58:01 +0000

    BACKPORT: bpf: Use BTF sock_common pointers for sk storage
    [ Upstream commit 592a3498648af000e93dff2d36229ab11cd8c7f6 ]
    
    This patch changes the bpf_sk_storage_*() to take
    ARG_PTR_TO_BTF_ID_SOCK_COMMON such that they will work with the pointer
    returned by the bpf_skc_to_*() helpers also.
    
    A micro benchmark has been done on a "cgroup_skb/egress" bpf program
    which does a bpf_sk_storage_get().  It was driven by netperf doing
    a 4096 connected UDP_STREAM test with 64bytes packet.
    The stats from "kernel.bpf_stats_enabled" shows no meaningful difference.
    
    The sk_storage_get_btf_proto, sk_storage_delete_btf_proto,
    btf_sk_storage_get_proto, and btf_sk_storage_delete_proto are
    no longer needed, so they are removed.
    
    [Mathias Gluszczynski: Apply the common sk-storage prototype and
    runtime full-socket checks to the Linux 4.9 local-storage
    implementation. Retain the CGROUP_SOCK-specific ARG_PTR_TO_CTX
    prototype and omit LSM and TCP congestion-control cleanup that is not
    present at this point in the series.]
    
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Lorenz Bauer <lmb@cloudflare.com>
    Link: https://lore.kernel.org/bpf/20200925000402.3856307-1-kafai@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/bpf_sk_storage.c
+++ net/core/bpf_sk_storage.c
@@ -743,7 +743,8 @@ BPF_CALL_4(bpf_sk_storage_get, struct bpf_map *, map, struct sock *, sk,
 {
 	struct bpf_sk_storage_data *sdata;
 
-	if (flags > BPF_SK_STORAGE_GET_F_CREATE)
+	if (!sk || !sk_fullsock(sk) ||
+	    flags > BPF_SK_STORAGE_GET_F_CREATE)
 		return (unsigned long)NULL;
 
 	sdata = sk_storage_lookup(sk, map, true);
@@ -771,6 +772,9 @@ BPF_CALL_4(bpf_sk_storage_get, struct bpf_map *, map, struct sock *, sk,
 
 BPF_CALL_2(bpf_sk_storage_delete, struct bpf_map *, map, struct sock *, sk)
 {
+	if (!sk || !sk_fullsock(sk))
+		return -EINVAL;
+
 	if (atomic_inc_not_zero(&sk->sk_refcnt)) {
 		int err;
 
@@ -798,7 +802,7 @@ const struct bpf_func_proto bpf_sk_storage_get_proto = {
 	.gpl_only	= false,
 	.ret_type	= RET_PTR_TO_MAP_VALUE_OR_NULL,
 	.arg1_type	= ARG_CONST_MAP_PTR,
-	.arg2_type	= ARG_PTR_TO_SOCKET,
+	.arg2_type	= ARG_PTR_TO_BTF_ID_SOCK_COMMON,
 	.arg3_type	= ARG_PTR_TO_MAP_VALUE_OR_NULL,
 	.arg4_type	= ARG_ANYTHING,
 };
@@ -818,5 +822,5 @@ const struct bpf_func_proto bpf_sk_storage_delete_proto = {
 	.gpl_only	= false,
 	.ret_type	= RET_INTEGER,
 	.arg1_type	= ARG_CONST_MAP_PTR,
-	.arg2_type	= ARG_PTR_TO_SOCKET,
+	.arg2_type	= ARG_PTR_TO_BTF_ID_SOCK_COMMON,
 };
