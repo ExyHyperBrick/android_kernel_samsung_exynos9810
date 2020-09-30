893109dacded1902cfeaf734a88ade7733024812
Daniel Borkmann <daniel@iogearbox.net>
2020-09-30 17:18:15 +0200

    BACKPORT: bpf: Add classid helper only based on skb->sk
    [ Upstream commit b426ce83baa7dff947fb354118d3133f2953aac8 ]
    
    Similarly to 5a52ae4e32a6 ("bpf: Allow to retrieve cgroup v1 classid
    from v2 hooks"), add a helper to retrieve cgroup v1 classid solely
    based on the skb->sk, so it can be used as key as part of BPF map
    lookups out of tc from host ns, in particular given the skb->sk is
    retained these days when crossing net ns thanks to 9c4c325252c5
    ("skbuff: preserve sock reference when scrubbing the skb."). This
    is similar to bpf_skb_cgroup_id() which implements the same for v2.
    Kubernetes ecosystem is still operating on v1 however, hence net_cls
    needs to be used there until this can be dropped in with the v2
    helper of bpf_skb_cgroup_id().
    
    [Mathias Gluszczynski: Registered the helper in the Exynos9810 4.9 TC
    dispatcher while retaining the existing configuration guard.]
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/ed633cf27a1c620e901c5aa99ebdefb028dce600.1601477936.git.daniel@iogearbox.net
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/filter.c
+++ net/core/filter.c
@@ -2495,6 +2495,25 @@ static const struct bpf_func_proto bpf_get_cgroup_classid_proto = {
 	.arg1_type      = ARG_PTR_TO_CTX,
 };
 
+#ifdef CONFIG_CGROUP_NET_CLASSID
+BPF_CALL_1(bpf_skb_cgroup_classid, const struct sk_buff *, skb)
+{
+	struct sock *sk = skb_to_full_sk(skb);
+
+	if (!sk || !sk_fullsock(sk))
+		return 0;
+
+	return sock_cgroup_classid(&sk->sk_cgrp_data);
+}
+
+static const struct bpf_func_proto bpf_skb_cgroup_classid_proto = {
+	.func		= bpf_skb_cgroup_classid,
+	.gpl_only	= false,
+	.ret_type	= RET_INTEGER,
+	.arg1_type	= ARG_PTR_TO_CTX,
+};
+#endif
+
 BPF_CALL_1(bpf_get_route_realm, const struct sk_buff *, skb)
 {
 	return dst_tclassid(skb);
@@ -5023,6 +5042,10 @@ tc_cls_act_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return &bpf_clone_redirect_proto;
 	case BPF_FUNC_get_cgroup_classid:
 		return &bpf_get_cgroup_classid_proto;
+#ifdef CONFIG_CGROUP_NET_CLASSID
+	case BPF_FUNC_skb_cgroup_classid:
+		return &bpf_skb_cgroup_classid_proto;
+#endif
 	case BPF_FUNC_skb_vlan_push:
 		return &bpf_skb_vlan_push_proto;
 	case BPF_FUNC_skb_vlan_pop:
