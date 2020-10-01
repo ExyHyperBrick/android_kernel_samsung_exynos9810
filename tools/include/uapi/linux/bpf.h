19dde76e9ac9b1a96a469fbc021d20696466a71d
Song Liu <songliubraving@fb.com>
2020-09-30 23:18:12 -0700

    BACKPORT: bpf: Introduce BPF_F_PRESERVE_ELEMS for perf event array
    [ Upstream commit 792caccc4526bb489e054f9ab61d7c024b15dea2 ]
    
    Currently, perf event in perf event array is removed from the array when
    the map fd used to add the event is closed. This behavior makes it
    difficult to the share perf events with perf event array.
    
    Introduce perf event map that keeps the perf event open with a new flag
    BPF_F_PRESERVE_ELEMS. With this flag set, perf events in the array are not
    removed when the original map fd is closed. Instead, the perf event will
    stay in the map until 1) it is explicitly removed from the array; or 2)
    the array is freed.
    
    [Mathias Gluszczynski: Integrated the flag with the Exynos9810 4.9
    combined array allocation check and synchronized both UAPI copies.]
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200930224927.1936644-2-songliubraving@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -395,6 +395,9 @@ enum bpf_stats_type {
 /* Enable memory-mapping BPF map */
 #define BPF_F_MMAPABLE		(1U << 10)
 
+/* Share perf_event among processes */
+#define BPF_F_PRESERVE_ELEMS	(1U << 11)
+
 union bpf_attr {
 	struct { /* anonymous struct used by BPF_MAP_CREATE command */
 		__u32	map_type;	/* one of enum bpf_map_type */
