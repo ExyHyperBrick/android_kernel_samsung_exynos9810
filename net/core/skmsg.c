bbd1ae0d5ba0b13e615a1dad73c2b65c2b59bc0b
John Fastabend <john.fastabend@gmail.com>
2020-10-09 11:37:55 -0700

    BACKPORT: bpf: Account sockmap ingress skb memory
    [ Upstream commit 0b17ad25d8d102ffb83dcc99ebd24719194ebf4f ]
    
    Move skb->sk assignment out of sk_psock_bpf_run() and into individual
    callers. Then we can use proper skb_set_owner_r() call to assign a
    sk to a skb. This improves things by also charging the truesize against
    the sockets sk_rmem_alloc counter. With this done we get some accounting
    in place to ensure the memory associated with skbs on the workqueue are
    still being accounted for somewhere. Finally, by using skb_set_owner_r
    the destructor is setup so we can just let the normal skb_kfree logic
    recover the memory. Combined with previous patch dropping skb_orphan()
    we now can recover from memory pressure and maintain accounting.
    
    Note, we will charge the skbs against their originating socket even
    if being redirected into another socket. Once the skb completes the
    redirect op the kfree_skb will give the memory back. This is important
    because if we charged the socket we are redirecting to (like it was
    done before this series) the sock_writeable() test could fail because
    of the skb trying to be sent is already charged against the socket.
    
    Also TLS case is special. Here we wait until we have decided not to
    simply PASS the packet up the stack. In the case where we PASS the
    packet up the stack we already have an skb which is accounted for on
    the TLS socket context.
    
    For the parser case we continue to just set/clear skb->sk this is
    because the skb being used here may be combined with other skbs or
    turned into multiple skbs depending on the parser logic. For example
    the parser could request a payload length greater than skb->len so
    that the strparser needs to collect multiple skbs. At any rate
    the final result will be handled in the strparser recv callback.
    
    [Mathias Gluszczynski: Update the stream-parser ownership
    paths present in this tree; the later TLS verdict integration is
    intentionally absent.]
    
    Fixes: 604326b41a6fb ("bpf, sockmap: convert to generic sk_msg interface")
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/160226867513.5692.10579573214635925960.stgit@john-Precision-5820-Tower
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/skmsg.c
+++ net/core/skmsg.c
@@ -623,20 +623,8 @@ EXPORT_SYMBOL_GPL(sk_psock_msg_verdict);
 static int sk_psock_bpf_run(struct sk_psock *psock, struct bpf_prog *prog,
 			    struct sk_buff *skb)
 {
-	int ret;
-
-	/* strparser clones the skb before handing it to a upper layer,
-	 * meaning we have the same data, but sk is NULL. We do want an
-	 * sk pointer though when we run the BPF program. So we set it
-	 * here and then NULL it to ensure we don't trigger a BUG_ON()
-	 * in skb/sk operations later if kfree_skb is called with a
-	 * valid skb->sk pointer and no destructor assigned.
-	 */
-	skb->sk = psock->sk;
 	bpf_compute_data_end_sk_skb(skb);
-	ret = bpf_prog_run_pin_on_cpu(prog, skb);
-	skb->sk = NULL;
-	return ret;
+	return bpf_prog_run_pin_on_cpu(prog, skb);
 }
 
 static struct sk_psock *sk_psock_from_strp(struct strparser *strp)
@@ -720,6 +708,7 @@ static void sk_psock_strp_read(struct strparser *strp, struct sk_buff *skb)
 		kfree_skb(skb);
 		goto out;
 	}
+	skb_set_owner_r(skb, sk);
 	prog = READ_ONCE(psock->progs.skb_verdict);
 	if (likely(prog)) {
 		tcp_skb_bpf_redirect_clear(skb);
@@ -744,8 +733,11 @@ static int sk_psock_strp_parse(struct strparser *strp, struct sk_buff *skb)
 
 	rcu_read_lock();
 	prog = READ_ONCE(psock->progs.skb_parser);
-	if (likely(prog))
+	if (likely(prog)) {
+		skb->sk = psock->sk;
 		ret = sk_psock_bpf_run(psock, prog, skb);
+		skb->sk = NULL;
+	}
 	rcu_read_unlock();
 	return ret;
 }
