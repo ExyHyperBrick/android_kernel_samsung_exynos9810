07a57436a751e0494c3d2544099fe5e2698b0db7
Minchan Kim <minchan@kernel.org>
2020-10-17 16:14:50 -0700

    BACKPORT: mm/madvise: pass mm to do_madvise
    [ Upstream commit 0726b01e70455f9900ab524117c7b520d197dc8c ]
    
    Patch series "introduce memory hinting API for external process", v9.
    
    Now, we have MADV_PAGEOUT and MADV_COLD as madvise hinting API.  With
    that, application could give hints to kernel what memory range are
    preferred to be reclaimed.  However, in some platform(e.g., Android), the
    information required to make the hinting decision is not known to the app.
    Instead, it is known to a centralized userspace daemon(e.g.,
    ActivityManagerService), and that daemon must be able to initiate reclaim
    on its own without any app involvement.
    
    To solve the concern, this patch introduces new syscall -
    process_madvise(2).  Bascially, it's same with madvise(2) syscall but it
    has some differences.
    
    1. It needs pidfd of target process to provide the hint
    
    2. It supports only MADV_{COLD|PAGEOUT|MERGEABLE|UNMEREABLE} at this
       moment.  Other hints in madvise will be opened when there are explicit
       requests from community to prevent unexpected bugs we couldn't support.
    
    3. Only privileged processes can do something for other process's
       address space.
    
    For more detail of the new API, please see "mm: introduce external memory
    hinting API" description in this patchset.
    
    This patch (of 3):
    
    In upcoming patches, do_madvise will be called from external process
    context so we shouldn't asssume "current" is always hinted process's
    task_struct.
    
    Furthermore, we must not access mm_struct via task->mm, but obtain it via
    access_mm() once (in the following patch) and only use that pointer [1],
    so pass it to do_madvise() as well.  Note the vma->vm_mm pointers are
    safe, so we can use them further down the call stack.
    
    And let's pass current->mm as arguments of do_madvise so it shouldn't
    change existing behavior but prepare next patch to make review easy.
    
    [vbabka@suse.cz: changelog tweak]
    [minchan@kernel.org: use current->mm for io_uring]
      Link: http://lkml.kernel.org/r/20200423145215.72666-1-minchan@kernel.org
    [akpm@linux-foundation.org: fix it for upstream changes]
    [akpm@linux-foundation.org: whoops]
    [rdunlap@infradead.org: add missing includes]
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Jann Horn <jannh@google.com>
    Cc: Tim Murray <timmurray@google.com>
    Cc: Daniel Colascione <dancol@google.com>
    Cc: Sandeep Patil <sspatil@google.com>
    Cc: Sonny Rao <sonnyrao@google.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: John Dias <joaodias@google.com>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Cc: SeongJae Park <sj38.park@gmail.com>
    Cc: Christian Brauner <christian@brauner.io>
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Cc: Oleksandr Natalenko <oleksandr@redhat.com>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: Florian Weimer <fw@deneb.enyo.de>
    Cc: <linux-man@vger.kernel.org>
    Link: https://lkml.kernel.org/r/20200901000633.1920247-1-minchan@kernel.org
    Link: http://lkml.kernel.org/r/20200622192900.22757-1-minchan@kernel.org
    Link: http://lkml.kernel.org/r/20200302193630.68771-2-minchan@kernel.org
    Link: http://lkml.kernel.org/r/20200622192900.22757-2-minchan@kernel.org
    Link: https://lkml.kernel.org/r/20200901000633.1920247-2-minchan@kernel.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    [Mathias Gluszczynski: Extract do_madvise locally and pass the target mm
    through the 4.9 mmap_sem paths. The older WILLNEED path does not drop
    mmap_sem; this tree has no io_uring madvise caller.]
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- mm/madvise.c
+++ mm/madvise.c
@@ -756,6 +756,7 @@ static long madvise_dontneed_free(struct vm_area_struct *vma,
 				  unsigned long start, unsigned long end,
 				  int behavior)
 {
+	struct mm_struct *mm = vma->vm_mm;
 	*prev = vma;
 	if (!can_madv_lru_vma(vma))
 		return -EINVAL;
@@ -763,8 +764,8 @@ static long madvise_dontneed_free(struct vm_area_struct *vma,
 	if (!userfaultfd_remove(vma, start, end)) {
 		*prev = NULL; /* mmap_sem has been dropped, prev is stale */
 
-		down_read(&current->mm->mmap_sem);
-		vma = find_vma(current->mm, start);
+		down_read(&mm->mmap_sem);
+		vma = find_vma(mm, start);
 		if (!vma)
 			return -ENOMEM;
 		if (start < vma->vm_start) {
@@ -815,6 +816,7 @@ static long madvise_remove(struct vm_area_struct *vma,
 				struct vm_area_struct **prev,
 				unsigned long start, unsigned long end)
 {
+	struct mm_struct *mm = vma->vm_mm;
 	loff_t offset;
 	int error;
 	struct file *f;
@@ -845,13 +847,13 @@ static long madvise_remove(struct vm_area_struct *vma,
 	get_file(f);
 	if (userfaultfd_remove(vma, start, end)) {
 		/* mmap_sem was not released by userfaultfd_remove() */
-		up_read(&current->mm->mmap_sem);
+		up_read(&mm->mmap_sem);
 	}
 	error = vfs_fallocate(f,
 				FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
 				offset, end - start);
 	fput(f);
-	down_read(&current->mm->mmap_sem);
+	down_read(&mm->mmap_sem);
 	return error;
 }
 
@@ -1021,7 +1023,8 @@ madvise_behavior_valid(int behavior)
  *  -EBADF  - map exists, but area maps something that isn't a file.
  *  -EAGAIN - a kernel resource was temporarily unavailable.
  */
-SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
+static int do_madvise(struct mm_struct *mm, unsigned long start,
+		      size_t len_in, int behavior)
 {
 	unsigned long end, tmp;
 	struct vm_area_struct *vma, *prev;
@@ -1056,10 +1059,10 @@ SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
 
 	write = madvise_need_mmap_write(behavior);
 	if (write) {
-		if (down_write_killable(&current->mm->mmap_sem))
+		if (down_write_killable(&mm->mmap_sem))
 			return -EINTR;
 	} else {
-		down_read(&current->mm->mmap_sem);
+		down_read(&mm->mmap_sem);
 	}
 
 	/*
@@ -1067,7 +1070,7 @@ SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
 	 * ranges, just ignore them, but return -ENOMEM at the end.
 	 * - different from the way of handling in mlock etc.
 	 */
-	vma = find_vma_prev(current->mm, start, &prev);
+	vma = find_vma_prev(mm, start, &prev);
 	if (vma && start > vma->vm_start)
 		prev = vma;
 
@@ -1104,14 +1107,19 @@ SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
 		if (prev)
 			vma = prev->vm_next;
 		else	/* madvise_remove dropped mmap_sem */
-			vma = find_vma(current->mm, start);
+			vma = find_vma(mm, start);
 	}
 out:
 	blk_finish_plug(&plug);
 	if (write)
-		up_write(&current->mm->mmap_sem);
+		up_write(&mm->mmap_sem);
 	else
-		up_read(&current->mm->mmap_sem);
+		up_read(&mm->mmap_sem);
 
 	return error;
+}
+
+SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
+{
+	return do_madvise(current->mm, start, len_in, behavior);
 }
