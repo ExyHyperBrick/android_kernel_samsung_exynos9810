6726d9f67f18929efeb41792d7792910dc0aae65
Stanislav Fomichev <sdf@google.com>
2021-02-26 12:49:19 -0800

    BACKPORT: bpf: Allow rewriting to ports under ip_unprivileged_port_start
    At the moment, BPF_CGROUP_INET{4,6}_BIND hooks can rewrite user_port
    to the privileged ones (< ip_unprivileged_port_start), but it will
    be rejected later on in the __inet_bind or __inet6_bind.
    
    Let's add another return value to indicate that CAP_NET_BIND_SERVICE
    check should be ignored. Use the same idea as we currently use
    in cgroup/egress where bit #1 indicates CN. Instead, for
    cgroup/bind{4,6}, bit #1 indicates that CAP_NET_BIND_SERVICE should
    be bypassed.
    
    v5:
    - rename flags to be less confusing (Andrey Ignatov)
    - rework BPF_PROG_CGROUP_INET_EGRESS_RUN_ARRAY to work on flags
      and accept BPF_RET_SET_CN (no behavioral changes)
    
    v4:
    - Add missing IPv6 support (Martin KaFai Lau)
    
    v3:
    - Update description (Martin KaFai Lau)
    - Fix capability restore in selftest (Martin KaFai Lau)
    
    v2:
    - Switch to explicit return code (Martin KaFai Lau)
    
    On this 4.9 tree, convert the legacy pair of bind booleans to the
    upstream flags interface. Keep the tree's existing preemption and
    cgroup-storage handling in BPF_PROG_RUN_ARRAY_FLAGS.
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Reviewed-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Andrey Ignatov <rdna@fb.com>
    Link: https://lore.kernel.org/bpf/20210127193140.3170382-1-sdf@google.com
    (cherry picked from commit 772412176fb98493158929b220fe250127f611af)
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- include/linux/bpf-cgroup.h
+++ include/linux/bpf-cgroup.h
@@ -117,7 +117,8 @@ int __cgroup_bpf_run_filter_sk(struct sock *sk,
 int __cgroup_bpf_run_filter_sock_addr(struct sock *sk,
 				      struct sockaddr *uaddr,
 				      enum bpf_attach_type type,
-				      void *t_ctx);
+				      void *t_ctx,
+				      u32 *flags);
 
 int __cgroup_bpf_run_filter_sock_ops(struct sock *sk,
 				     struct bpf_sock_ops_kern *sock_ops,
@@ -219,30 +220,44 @@ int bpf_percpu_cgroup_storage_update(struct bpf_map *map, void *key,
 
 #define BPF_CGROUP_RUN_SA_PROG(sk, uaddr, type)				       \
 ({									       \
+	u32 __unused_flags = 0;						       \
 	int __ret = 0;							       \
 	if (cgroup_bpf_enabled)						       \
 		__ret = __cgroup_bpf_run_filter_sock_addr(sk, uaddr, type,     \
-							  NULL);	       \
+							  NULL,		       \
+							  &__unused_flags);    \
 	__ret;								       \
 })
 
 #define BPF_CGROUP_RUN_SA_PROG_LOCK(sk, uaddr, type, t_ctx)		       \
 ({									       \
+	u32 __unused_flags = 0;						       \
 	int __ret = 0;							       \
 	if (cgroup_bpf_enabled)	{					       \
 		lock_sock(sk);						       \
 		__ret = __cgroup_bpf_run_filter_sock_addr(sk, uaddr, type,     \
-							  t_ctx);	       \
+							  t_ctx,	       \
+							  &__unused_flags);    \
 		release_sock(sk);					       \
 	}								       \
 	__ret;								       \
 })
 
-#define BPF_CGROUP_RUN_PROG_INET4_BIND(sk, uaddr)			       \
-	BPF_CGROUP_RUN_SA_PROG(sk, uaddr, BPF_CGROUP_INET4_BIND)
-
-#define BPF_CGROUP_RUN_PROG_INET6_BIND(sk, uaddr)			       \
-	BPF_CGROUP_RUN_SA_PROG(sk, uaddr, BPF_CGROUP_INET6_BIND)
+/* BPF_CGROUP_INET{4,6}_BIND can return extra flags in the upper bits
+ * of the return code. Bit 0 asks bind to bypass CAP_NET_BIND_SERVICE.
+ */
+#define BPF_CGROUP_RUN_PROG_INET_BIND(sk, uaddr, type, bind_flags)	       \
+({									       \
+	u32 __flags = 0;						       \
+	int __ret = 0;							       \
+	if (cgroup_bpf_enabled) {					       \
+		__ret = __cgroup_bpf_run_filter_sock_addr(sk, uaddr, type,     \
+							  NULL, &__flags);     \
+		if (__flags & BPF_RET_BIND_NO_CAP_NET_BIND_SERVICE)	       \
+			*(bind_flags) |= BIND_NO_CAP_NET_BIND_SERVICE;	       \
+	}								       \
+	__ret;								       \
+})
 
 #define BPF_CGROUP_PRE_CONNECT_ENABLED(sk) (cgroup_bpf_enabled && \
 					    sk->sk_prot->pre_connect)
@@ -369,8 +384,7 @@ static inline int bpf_percpu_cgroup_storage_update(struct bpf_map *map,
 #define BPF_CGROUP_RUN_PROG_INET_EGRESS(sk,skb) ({ 0; })
 #define BPF_CGROUP_RUN_PROG_INET_SOCK(sk) ({ 0; })
 #define BPF_CGROUP_RUN_PROG_INET_SOCK_RELEASE(sk) ({ 0; })
-#define BPF_CGROUP_RUN_PROG_INET4_BIND(sk, uaddr) ({ 0; })
-#define BPF_CGROUP_RUN_PROG_INET6_BIND(sk, uaddr) ({ 0; })
+#define BPF_CGROUP_RUN_PROG_INET_BIND(sk, uaddr, type, flags) ({ 0; })
 #define BPF_CGROUP_RUN_PROG_INET4_POST_BIND(sk) ({ 0; })
 #define BPF_CGROUP_RUN_PROG_INET6_POST_BIND(sk) ({ 0; })
 #define BPF_CGROUP_RUN_PROG_INET4_CONNECT(sk, uaddr) ({ 0; })
