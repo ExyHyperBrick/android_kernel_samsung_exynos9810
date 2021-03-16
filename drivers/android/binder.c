e5dba8a19d084ed5d26e120bc5d472d32950b7e8
Marco Ballesio <balejs@google.com>
2021-03-15 18:16:30 -0700

    BACKPORT: binder: BINDER_GET_FROZEN_INFO ioctl
    [ Upstream commit ae28c1be1e54f2eda1c8b4469c4652e8a24056ed ]
    
    User space needs to know if binder transactions occurred to frozen
    processes. Introduce a new BINDER_GET_FROZEN ioctl and keep track of
    transactions occurring to frozen proceses.
    
    Signed-off-by: Marco Ballesio <balejs@google.com>
    Signed-off-by: Li Li <dualli@google.com>
    Acked-by: Todd Kjos <tkjos@google.com>
    Link: https://lore.kernel.org/r/20210316011630.1121213-4-dualli@chromium.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    [Mathias Gluszczynski: Add the receive-state fields to binder_proc in
    binder.c. Preserve the existing higher-numbered spam-detection ioctl.]
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -510,6 +510,8 @@ struct binder_priority {
  *                        (protected by binder_deferred_lock)
  * @outstanding_txns:     transactions pending completion (@inner_lock)
  * @is_frozen:            unable to service transactions (@inner_lock)
+ * @sync_recv:            sync transactions since freezing (@inner_lock)
+ * @async_recv:           async transactions since freezing (@inner_lock)
  * @freeze_wait:          waiters for outstanding transactions
  * @is_dead:              process is dead and awaiting free
  *                        when outstanding transactions are cleaned up
@@ -561,6 +563,8 @@ struct binder_proc {
 	int deferred_work;
 	int outstanding_txns;
 	bool is_frozen;
+	bool sync_recv;
+	bool async_recv;
 	wait_queue_head_t freeze_wait;
 	bool is_dead;
 
@@ -2970,6 +2974,10 @@ static int binder_proc_transaction(struct binder_transaction *t,
 	}
 
 	binder_inner_proc_lock(proc);
+	if (proc->is_frozen) {
+		proc->sync_recv |= !oneway;
+		proc->async_recv |= oneway;
+	}
 
 	if ((proc->is_frozen && !oneway) || proc->is_dead ||
 			(thread && thread->is_dead)) {
@@ -5098,6 +5106,8 @@ static int binder_ioctl_freeze(struct binder_freeze_info *info,
 
 	if (!info->enable) {
 		binder_inner_proc_lock(target_proc);
+		target_proc->sync_recv = false;
+		target_proc->async_recv = false;
 		target_proc->is_frozen = false;
 		binder_inner_proc_unlock(target_proc);
 		return 0;
@@ -5109,6 +5119,8 @@ static int binder_ioctl_freeze(struct binder_freeze_info *info,
 	 * for transactions to drain.
 	 */
 	binder_inner_proc_lock(target_proc);
+	target_proc->sync_recv = false;
+	target_proc->async_recv = false;
 	target_proc->is_frozen = true;
 	binder_inner_proc_unlock(target_proc);
 
@@ -5128,6 +5140,33 @@ static int binder_ioctl_freeze(struct binder_freeze_info *info,
 	}
 
 	return ret;
+}
+
+static int binder_ioctl_get_freezer_info(
+				struct binder_frozen_status_info *info)
+{
+	struct binder_proc *target_proc;
+	bool found = false;
+
+	info->sync_recv = 0;
+	info->async_recv = 0;
+
+	mutex_lock(&binder_procs_lock);
+	hlist_for_each_entry(target_proc, &binder_procs, proc_node) {
+		if (target_proc->pid == info->pid) {
+			found = true;
+			binder_inner_proc_lock(target_proc);
+			info->sync_recv |= target_proc->sync_recv;
+			info->async_recv |= target_proc->async_recv;
+			binder_inner_proc_unlock(target_proc);
+		}
+	}
+	mutex_unlock(&binder_procs_lock);
+
+	if (!found)
+		return -EINVAL;
+
+	return 0;
 }
 
 static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
@@ -5327,6 +5366,24 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		if (ret < 0)
 			goto err;
+		break;
+	}
+	case BINDER_GET_FROZEN_INFO: {
+		struct binder_frozen_status_info info;
+
+		if (copy_from_user(&info, ubuf, sizeof(info))) {
+			ret = -EFAULT;
+			goto err;
+		}
+
+		ret = binder_ioctl_get_freezer_info(&info);
+		if (ret < 0)
+			goto err;
+
+		if (copy_to_user(ubuf, &info, sizeof(info))) {
+			ret = -EFAULT;
+			goto err;
+		}
 		break;
 	}
 	default:
@@ -5667,6 +5724,8 @@ static void binder_deferred_release(struct binder_proc *proc)
 
 	proc->is_dead = true;
 	proc->is_frozen = false;
+	proc->sync_recv = false;
+	proc->async_recv = false;
 	threads = 0;
 	active_transactions = 0;
 	while ((n = rb_first(&proc->threads))) {
