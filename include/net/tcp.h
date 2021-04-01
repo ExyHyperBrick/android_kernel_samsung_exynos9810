a739ba29def4c66359f392c58339c3612a2dbb8c
Cong Wang <cong.wang@bytedance.com>
2021-04-01 10:56:14 -0700

    BACKPORT: skmsg: Extract common receive and wait helpers
    [ Upstream commit 2bc793e3272a13e337416c057cb81c5396ad91d1 ]
    
    Although these two functions are only used by TCP, they are not
    specific to TCP at all, both operate on skmsg and ingress_msg,
    so fit in net/core/skmsg.c very well.
    
    And we will need them for non-TCP, so rename and move them to
    skmsg.c and export them to modules.
    
    [Mathias Gluszczynski: Move Linux 4.9's TCP receive and wait
    helpers into skmsg.c, retain its sk_wait_event() calling convention,
    and add the upstream MSG_PEEK handling.]
    
    Signed-off-by: Cong Wang <cong.wang@bytedance.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210331023237.41094-13-xiyou.wangcong@gmail.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2127,8 +2127,6 @@ int tcp_bpf_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 int tcp_bpf_recvmsg_parser(struct sock *sk, struct msghdr *msg, size_t len,
 			   int nonblock, int flags, int *addr_len);
 
-int __tcp_bpf_recvmsg(struct sock *sk, struct sk_psock *psock,
-		      struct msghdr *msg, int len);
 
 /* Call BPF_SOCK_OPS program that returns an int. If the return value
  * is < 0, then the BPF op failed (for example if the loaded BPF
