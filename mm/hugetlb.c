7e8d3609be70a068f84ece6dd06bbcee8d174dc5
Miaohe Lin <linmiaohe@huawei.com>
2022-07-12 21:05:42 +0800

    BACKPORT: hugetlb: preserve cached page mappings in UFFDIO_CONTINUE
    Original subject: mm/hugetlb: avoid corrupting page->mapping in
    hugetlb_mcopy_atomic_pte
    
    In MCOPY_ATOMIC_CONTINUE case with a non-shared VMA, pages in the page
    cache are installed in the ptes.  But hugepage_add_new_anon_rmap is
    called for them mistakenly because they're not vm_shared.  This will
    corrupt the page->mapping used by page cache code.
    
    Link:
     https://lkml.kernel.org/r/20220712130542.18836-1-linmiaohe@huawei.com
    Fixes: f619147104c8 ("userfaultfd: add UFFDIO_CONTINUE ioctl")
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    [Mathias Gluszczynski: Use vm_shared or is_continue to identify a cached
    page without the newer reservation-state variable. Retain
    page_dup_rmap() and ClearPagePrivate() from 4.9.]
    
    Upstream-commit: ab74ef708dc51df7cf2b8a890b9c6990fac5c0c6
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -4242,7 +4242,7 @@ int hugetlb_mcopy_atomic_pte(struct mm_struct *dst_mm,
 	if (!huge_pte_none(huge_ptep_get(dst_pte)))
 		goto out_release_unlock;
 
-	if (vm_shared) {
+	if (vm_shared || is_continue) {
 		page_dup_rmap(page, true);
 	} else {
 		ClearPagePrivate(page);
