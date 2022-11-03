66cdc980dbd8a39620789a87c6317bed98459807
Youlin Li <liulin063@gmail.com>
2022-11-03 17:34:39 +0800

    BACKPORT: bpf: Fix wrong reg type conversion in release_reference()
    [ Upstream commit f1db20814af532f85e091231223e5e4818e8464b ]
    
    Some helper functions will allocate memory. To avoid memory leaks, the
    verifier requires the eBPF program to release these memories by calling
    the corresponding helper functions.
    
    When a resource is released, all pointer registers corresponding to the
    resource should be invalidated. The verifier use release_references() to
    do this job, by apply  __mark_reg_unknown() to each relevant register.
    
    It will give these registers the type of SCALAR_VALUE. A register that
    will contain a pointer value at runtime, but of type SCALAR_VALUE, which
    may allow the unprivileged user to get a kernel pointer by storing this
    register into a map.
    
    Using __mark_reg_not_init() while NOT allow_ptr_leaks can mitigate this
    problem.
    
    [Mathias Gluszczynski: Apply the same privileged/unprivileged
    invalidation choice in this verifier's explicit register and spill
    traversal across every active frame.]
    
    Fixes: fd978bf7fd31 ("bpf: Add reference tracking to verifier")
    Signed-off-by: Youlin Li <liulin063@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20221103093440.3161-1-liulin063@gmail.com
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -4481,14 +4481,23 @@ static void release_reg_references(struct bpf_verifier_env *env,
 	struct bpf_reg_state *regs = state->regs, *reg;
 	int i;
 
-	for (i = 0; i < MAX_BPF_REG; i++)
-		if (regs[i].ref_obj_id == ref_obj_id)
+	for (i = 0; i < MAX_BPF_REG; i++) {
+		if (regs[i].ref_obj_id != ref_obj_id)
+			continue;
+		if (!env->allow_ptr_leaks)
+			mark_reg_not_init(env, regs, i);
+		else
 			mark_reg_unknown(env, regs, i);
+	}
 
 	bpf_for_each_spilled_reg(i, state, reg) {
 		if (!reg)
 			continue;
-		if (reg->ref_obj_id == ref_obj_id)
+		if (reg->ref_obj_id != ref_obj_id)
+			continue;
+		if (!env->allow_ptr_leaks)
+			__mark_reg_not_init(env, reg);
+		else
 			__mark_reg_unknown(env, reg);
 	}
 }
