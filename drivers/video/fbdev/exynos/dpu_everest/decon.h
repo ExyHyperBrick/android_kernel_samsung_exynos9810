aa23642f57edcf39017ee160ec3e51a1cc5cdc60
Dylan Neve <dylanneve1@gmail.com>
2023-05-31 16:01:13 -0600

    drivers: fbdev: dpu_everest: create decon CAL for exynos9810
    SOC specific definitions moved to cal_9810/decon_cal.h. Anything in decon_regs should be moved to decon_cal for access since not necessarily same across SOC.
    
    Adapted from dpu20 cal_9810.h with mino adjustments. decon_reg_stop takes just 4 arguments, full dpu20 takes an extra argument for fps so this is removed. Also added missing definitions to decon_data_path and removed any unused definitions from dpu20.

--- drivers/video/fbdev/exynos/dpu_everest/decon.h
+++ drivers/video/fbdev/exynos/dpu_everest/decon.h
@@ -40,7 +40,11 @@
 #include "../panel/panel_drv.h"
 #endif
 
+/* TODO: SoC dependency will be removed */
+#if defined(CONFIG_SOC_EXYNOS9810)
 #include "./cal_9810/regs-decon.h"
+#include "./cal_9810/decon_cal.h"
+#endif
 
 #include "./panels/decon_lcd.h"
 #include "dsim.h"
@@ -53,7 +57,6 @@
 #include "disp_err.h"
 #endif
 
-#define MAX_DECON_CNT		3
 #define SUCCESS_EXYNOS_SMC	0
 
 #define DISP_RESTRICTION_VER	20180608
@@ -77,14 +80,11 @@ extern struct decon_bts_ops decon_bts_control;
 #define MIN_BLK_MODE_HEIGHT	16
 #define VSYNC_TIMEOUT_MSEC	200
 #define DEFAULT_BPP		32
-#define MAX_DECON_WIN		6
-#define MAX_DPP_SUBDEV		7
 #define MIN_WIN_BLOCK_WIDTH	8
 #define MIN_WIN_BLOCK_HEIGHT	1
 #define FD_TRY_CNT		3
 #define VALID_FD_VAL		3
 #define DECON_TRACE_BUF_SIZE	40
-#define CHIP_VER	(9810)
 
 #define DECON_WIN_UPDATE_IDX	(6)
 
@@ -188,25 +188,6 @@ void decon_tracing_mark_write(struct decon_device *decon, char id, char *str1, i
 			decon_tracing_mark_write(decon, id, str1, value);		\
 	} while (0)
 
-enum decon_trig_mode {
-	DECON_HW_TRIG = 0,
-	DECON_SW_TRIG
-};
-
-enum decon_out_type {
-	DECON_OUT_DSI = 0,
-	DECON_OUT_EDP,
-	DECON_OUT_DP,
-	DECON_OUT_WB
-};
-
-enum decon_dsi_mode {
-	DSI_MODE_SINGLE = 0,
-	DSI_MODE_DUAL_DSI,
-	DSI_MODE_DUAL_DISPLAY,
-	DSI_MODE_NONE
-};
-
 enum decon_hold_scheme {
 	/*  should be set to this value in case of DSIM video mode */
 	DECON_VCLK_HOLD_ONLY		= 0x00,
@@ -215,32 +196,6 @@ enum decon_hold_scheme {
 	DECON_VCLK_HOLD_VDEN_DISABLE	= 0x02,
 	/*  should be set to this value in case of HDMI, eDP */
 	DECON_VCLK_NOT_AFFECTED		= 0x03,
-};
-
-enum decon_rgb_order {
-	DECON_RGB = 0x0,
-	DECON_GBR = 0x1,
-	DECON_BRG = 0x2,
-	DECON_BGR = 0x4,
-	DECON_RBG = 0x5,
-	DECON_GRB = 0x6,
-};
-
-enum decon_win_func {
-	PD_FUNC_CLEAR			= 0x0,
-	PD_FUNC_COPY			= 0x1,
-	PD_FUNC_DESTINATION		= 0x2,
-	PD_FUNC_SOURCE_OVER		= 0x3,
-	PD_FUNC_DESTINATION_OVER	= 0x4,
-	PD_FUNC_SOURCE_IN		= 0x5,
-	PD_FUNC_DESTINATION_IN		= 0x6,
-	PD_FUNC_SOURCE_OUT		= 0x7,
-	PD_FUNC_DESTINATION_OUT		= 0x8,
-	PD_FUNC_SOURCE_A_TOP		= 0x9,
-	PD_FUNC_DESTINATION_A_TOP	= 0xa,
-	PD_FUNC_XOR			= 0xb,
-	PD_FUNC_PLUS			= 0xc,
-	PD_FUNC_USER_DEFINED		= 0xd,
 };
 
 enum decon_win_alpha_coef {
@@ -265,14 +220,6 @@ enum decon_win_alpha_sel {
 	ALPHA_MULT_SRC_SEL_AB = 3,
 };
 
-enum decon_fifo_mode {
-	DECON_FIFO_00K = 0,
-	DECON_FIFO_04K,
-	DECON_FIFO_08K,
-	DECON_FIFO_12K,
-	DECON_FIFO_16K,
-};
-
 enum decon_merger_mode {
 	DECON_LRM_NO		= 0x0,
 	DECON_LRM_NOSWAP_RF	= 0x4,
@@ -286,22 +233,6 @@ enum decon_te_src {
 	DECON_TE_FROM_DDI1,
 	DECON_TE_FROM_DDI2,
 	DECON_TE_FROM_USB,
-};
-
-enum decon_set_trig {
-	DECON_TRIG_DISABLE = 0,
-	DECON_TRIG_ENABLE
-};
-
-enum decon_idma_type {
-	IDMA_G0 = 0,
-	IDMA_G1,
-	IDMA_VG0,
-	IDMA_VG1,
-	IDMA_VGF0,
-	IDMA_VGF1, /* VGRF in case of Exynos9810 */
-	ODMA_WB,
-	MAX_DECON_DMA_TYPE,
 };
 
 /*
@@ -332,62 +263,10 @@ enum decon_clk_id {
 	CLK_ID_MAX,
 };
 
-enum decon_path_cfg {
-	PATH_CON_ID_DSIM_IF0 = 0,
-	PATH_CON_ID_DSIM_IF1 = 1,
-	PATH_CON_ID_DP = 3,
-	PATH_CON_ID_DUAL_DSC = 4,
-	PATH_CON_ID_DSCC_EN = 7,
-};
-
-enum decon_data_path {
-	/* No comp - OUTFIFO0 DSIM_IF0 */
-	DPATH_NOCOMP_OUTFIFO0_DSIMIF0			= 0x001,
-	/* No comp - FF0 - FORMATTER1 - DSIM_IF1 */
-	DPATH_NOCOMP_OUTFIFO0_DSIMIF1			= 0x002,
-	/* No comp - SPLITTER - FF0/1 - FORMATTER0/1 - DSIM_IF0/1 */
-	DPATH_NOCOMP_SPLITTER_OUTFIFO01_DSIMIF01	= 0x003,
-
-	/* DSC_ENC0 - OUTFIFO0 - DSIM_IF0 */
-	DPATH_DSCENC0_OUTFIFO0_DSIMIF0		= 0x011,
-	/* DSC_ENC0 - OUTFIFO0 - DSIM_IF1 */
-	DPATH_DSCENC0_OUTFIFO0_DSIMIF1		= 0x012,
-
-	/* DSCC,DSC_ENC0/1 - OUTFIFO01 DSIM_IF0 */
-	DPATH_DSCC_DSCENC01_OUTFIFO01_DSIMIF0	= 0x0B1,
-	/* DSCC,DSC_ENC0/1 - OUTFIFO01 DSIM_IF1 */
-	DPATH_DSCC_DSCENC01_OUTFIFO01_DSIMIF1	= 0x0B2,
-	/* DSCC,DSC_ENC0/1 - OUTFIFO01 DSIM_IF0/1*/
-	DPATH_DSCC_DSCENC01_OUTFIFO01_DSIMIF01	= 0x0B3,
-
-	/* WB_PRE */
-	DPATH_WBPRE_ONLY			= 0x100,
-
-	/* No comp - OUTFIFO0 DSIM_IF0 */
-	DECON1_NOCOMP_OUTFIFO0_DSIMIF0	= 0x001,
-	/* No comp - OUTFIFO0 DP_IF */
-	DECON1_NOCOMP_OUTFIFO0_DPIF		= 0x008,
-	/* DSC_ENC1 - OUTFIFO0 - DSIM_IF0 */
-	DECON1_DSCENC1_OUTFIFO0_DSIMIF0	= 0x021,
-	/* DSC_ENC1 - OUTFIFO0 - DP_IF */
-	DECON1_DSCENC1_OUTFIFO0_DPIF	= 0x028,
-
-	/* No comp - OUTFIFO0 DP_IF */
-	DECON2_NOCOMP_OUTFIFO0_DPIF		= 0x008,
-	/* DSC_ENC2 - OUTFIFO0 - DP_IF0 */
-	DECON2_DSCENC2_OUTFIFO0_DPIF	= 0x048,
-};
-
 enum decon_dsc_id {
 	DECON_DSC_ENC0 = 0x0,
 	DECON_DSC_ENC1 = 0x1,
 	DECON_DSC_ENC2 = 0x2,
-};
-
-enum decon_scaler_path {
-	SCALERPATH_OFF	= 0x0,
-	SCALERPATH_VGF	= 0x1,
-	SCALERPATH_VGRF	= 0x2,
 };
 
 enum decon_share_path {
@@ -396,72 +275,6 @@ enum decon_share_path {
 	SHAREPATH_VG1_USE		= 0x2,
 	SHAREPATH_VGF1_USE		= 0x3,
 	SHAREPATH_VGF0_USE		= 0x4,
-};
-
-enum decon_pixel_format {
-	/* RGB 8bit display */
-	/* 4byte */
-	DECON_PIXEL_FORMAT_ARGB_8888 = 0,
-	DECON_PIXEL_FORMAT_ABGR_8888,
-	DECON_PIXEL_FORMAT_RGBA_8888,
-	DECON_PIXEL_FORMAT_BGRA_8888,
-	DECON_PIXEL_FORMAT_XRGB_8888,
-	DECON_PIXEL_FORMAT_XBGR_8888,
-	DECON_PIXEL_FORMAT_RGBX_8888,
-	DECON_PIXEL_FORMAT_BGRX_8888,
-	/* 2byte */
-	DECON_PIXEL_FORMAT_RGBA_5551,
-	DECON_PIXEL_FORMAT_BGRA_5551,
-	DECON_PIXEL_FORMAT_ABGR_4444,
-	DECON_PIXEL_FORMAT_RGBA_4444,
-	DECON_PIXEL_FORMAT_BGRA_4444,
-	DECON_PIXEL_FORMAT_RGB_565,
-	DECON_PIXEL_FORMAT_BGR_565,
-
-	/* RGB 10bit display */
-	/* 4byte */
-	DECON_PIXEL_FORMAT_ARGB_2101010,
-	DECON_PIXEL_FORMAT_ABGR_2101010,
-	DECON_PIXEL_FORMAT_RGBA_1010102,
-	DECON_PIXEL_FORMAT_BGRA_1010102,
-
-	/* YUV 8bit display */
-	/* YUV422 2P */
-	DECON_PIXEL_FORMAT_NV16,
-	DECON_PIXEL_FORMAT_NV61,
-	/* YUV422 3P */
-	DECON_PIXEL_FORMAT_YVU422_3P,
-	/* YUV420 2P */
-	DECON_PIXEL_FORMAT_NV12,
-	DECON_PIXEL_FORMAT_NV21,
-	DECON_PIXEL_FORMAT_NV12M,
-	DECON_PIXEL_FORMAT_NV21M,
-	/* YUV420 3P */
-	DECON_PIXEL_FORMAT_YUV420,
-	DECON_PIXEL_FORMAT_YVU420,
-	DECON_PIXEL_FORMAT_YUV420M,
-	DECON_PIXEL_FORMAT_YVU420M,
-	/* YUV - 2 planes but 1 buffer */
-	DECON_PIXEL_FORMAT_NV12N,
-	DECON_PIXEL_FORMAT_NV12N_10B,
-
-	/* YUV 10bit display */
-	/* YUV420 2P */
-	DECON_PIXEL_FORMAT_NV12M_P010,
-	DECON_PIXEL_FORMAT_NV21M_P010,
-
-	/* YUV420(P8+2) 4P */
-	DECON_PIXEL_FORMAT_NV12M_S10B,
-	DECON_PIXEL_FORMAT_NV21M_S10B,
-
-	DECON_PIXEL_FORMAT_MAX,
-};
-
-enum decon_blending {
-	DECON_BLENDING_NONE = 0,
-	DECON_BLENDING_PREMULT = 1,
-	DECON_BLENDING_COVERAGE = 2,
-	DECON_BLENDING_MAX = 3,
 };
 
 enum dpp_rotate {
@@ -514,38 +327,6 @@ struct decon_color_mode_info {
 
 struct decon_clocks {
 	unsigned long decon[CLK_ID_DPLL + 1];
-};
-
-struct decon_mode_info {
-	enum decon_psr_mode psr_mode;
-	enum decon_trig_mode trig_mode;
-	enum decon_out_type out_type;
-	enum decon_dsi_mode dsi_mode;
-};
-
-struct decon_param {
-	struct decon_mode_info psr;
-	struct decon_lcd *lcd_info;
-	u32 nr_windows;
-	void __iomem *disp_ss_regs;
-};
-
-struct decon_window_regs {
-	u32 wincon;
-	u32 start_pos;
-	u32 end_pos;
-	u32 colormap;
-	u32 start_time;
-	u32 pixel_count;
-	u32 whole_w;
-	u32 whole_h;
-	u32 offset_x;
-	u32 offset_y;
-	u32 winmap_state;
-	enum decon_idma_type type;
-	int plane_alpha;
-	enum decon_pixel_format format;
-	enum decon_blending blend;
 };
 
 struct decon_dma_buf_data {
@@ -1588,20 +1369,8 @@ static inline bool IS_DECON_HIBER_STATE(struct decon_device *decon)
 }
 
 /* CAL APIs list */
-int decon_reg_init(u32 id, u32 dsi_idx, struct decon_param *p);
 //void decon_reg_init_probe(u32 id, u32 dsi_idx, struct decon_param *p);
-int decon_reg_start(u32 id, struct decon_mode_info *psr);
-int decon_reg_stop(u32 id, u32 dsi_idx, struct decon_mode_info *psr, bool rst);
-void decon_reg_release_resource(u32 id, struct decon_mode_info *psr);
-void decon_reg_set_int(u32 id, struct decon_mode_info *psr, u32 en);
-void decon_reg_set_window_control(u32 id, int win_idx,
-		struct decon_window_regs *regs, u32 winmap_en);
-int decon_reg_wait_update_done_and_mask(u32 id, struct decon_mode_info *psr,
-		u32 timeout);
-void decon_reg_set_trigger(u32 id, struct decon_mode_info *psr,
-		enum decon_set_trig en);
 int decon_reg_wait_for_update_timeout(u32 id, unsigned long timeout);
-int decon_reg_get_interrupt_and_clear(u32 id, u32 *ext_irq);
 void decon_reg_set_blender_bg_image_size(u32 id, enum decon_dsi_mode dsi_mode,
 		struct decon_lcd *lcd_info);
 void decon_reg_config_data_path_size(u32 id, u32 width, u32 height,
@@ -1609,23 +1378,13 @@ void decon_reg_config_data_path_size(u32 id, u32 width, u32 height,
 u32 dsc_get_dual_slice_mode(struct decon_lcd *lcd_info);
 u32 dsc_get_slice_mode_change(struct decon_lcd *lcd_info);
 void decon_reg_set_dispif_size(u32 id, u32 width, u32 height);
-void decon_reg_set_mres(u32 id, struct decon_param *p);
 void decon_reg_clear_int_all(u32 id);
 void decon_reg_all_win_shadow_update_req(u32 id);
 void decon_reg_update_req_window(u32 id, u32 win_idx);
-void decon_reg_update_req_window_mask(u32 id, u32 win_idx);
-void decon_reg_set_partial_update(u32 id, enum decon_dsi_mode dsi_mode,
-		struct decon_lcd *lcd_info, bool in_slice[],
-		u32 partial_w, u32 partial_h);
-int decon_reg_wait_idle_status_timeout(u32 id, unsigned long timeout);
-void decon_reg_set_start_crc(u32 id, u32 en);
-void decon_reg_set_select_crc_bits(u32 id, u32 bit_sel);
-void decon_reg_get_crc_data(u32 id, u32 *w0_data, u32 *w1_data);
 void decon_reg_set_win_enable(u32 id, u32 win_idx, u32 en);
 
 /* tui feature support external to security driver(gud) */
 int decon_tui_protection(bool tui_en);
-void decon_reg_update_req_global(u32 id);
 
 /* helper functions */
 int dpu_get_sd_by_drvname(struct decon_device *decon, char *drvname);
