c092a3425db8e23a6ba9d4c2ff533c90ae1525e3
Xu Kuohai <xukuohai@huawei.com>
2023-08-04 03:37:37 -0400

    BACKPORT: bpf, sockmap: Fix map type error in sock_map_del_link
    [ Upstream commit 7e96ec0e6605b69bb21bbf6c0ff9051e656ec2b1 ]
    
    sock_map_del_link() operates on both SOCKMAP and SOCKHASH, although
    both types have member named "progs", the offset of "progs" member in
    these two types is different, so "progs" should be accessed with the
    real map type.
    
    [Mathias Gluszczynski: The shared sock_map_progs() helper is
    defined after this older SOCKHASH layout, so add a forward declaration
    and use its paired skb_parser field for the live RX path.]
    
    Fixes: 604326b41a6f ("bpf, sockmap: convert to generic sk_msg interface")
    Signed-off-by: Xu Kuohai <xukuohai@huawei.com>
    Reviewed-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/r/20230804073740.194770-2-xukuohai@huaweicloud.com
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/sock_map.c
+++ net/core/sock_map.c
@@ -19,6 +19,8 @@ struct bpf_stab {
 	raw_spinlock_t lock;
 };
 
+static struct sk_psock_progs *sock_map_progs(struct bpf_map *map);
+
 #define SOCK_CREATE_FLAG_MASK				\
 	(BPF_F_NUMA_NODE | BPF_F_RDONLY | BPF_F_WRONLY)
 
@@ -117,9 +119,9 @@ static void sock_map_del_link(struct sock *sk,
 	list_for_each_entry_safe(link, tmp, &psock->link, list) {
 		if (link->link_raw == link_raw) {
 			struct bpf_map *map = link->map;
-			struct bpf_stab *stab = container_of(map, struct bpf_stab,
-							     map);
-			if (psock->parser.enabled && stab->progs.skb_parser)
+			struct sk_psock_progs *progs = sock_map_progs(map);
+
+			if (psock->parser.enabled && progs->skb_parser)
 				strp_stop = true;
 			list_del(&link->list);
 			sk_psock_free_link(link);
