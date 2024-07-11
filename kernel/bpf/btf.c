4ea557a406005bd8332e66827e74ef5536db9a8e
Alan Maguire <alan.maguire@oracle.com>
2024-07-11 19:23:21 +0100

    BACKPORT: bpf: Annotate BTF show format functions
    [ Upstream commit b3470da314fd8018ee237e382000c4154a942420 ]
    
    -Werror=suggest-attribute=format warns about two functions
    in kernel/bpf/btf.c [1]; add __printf() annotations to silence
    these warnings since for CONFIG_WERROR=y they will trigger
    build failures.
    
    [1] https://lore.kernel.org/bpf/a8b20c72-6631-4404-9e1f-0410642d7d20@gmail.com/
    
    Fixes: 31d0bc81637d ("bpf: Move to generic BTF show support, apply it to seq files/strings")
    Reported-by: Mirsad Todorovac <mtodorovac69@gmail.com>
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Tested-by: Mirsad Todorovac <mtodorovac69@yahoo.com>
    Link: https://lore.kernel.org/r/20240711182321.963667-1-alan.maguire@oracle.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -5139,8 +5139,8 @@ static void btf_type_show(const struct btf *btf, u32 type_id, void *obj,
 	btf_type_ops(t)->show(btf, t, type_id, obj, 0, show);
 }
 
-static void btf_seq_show(struct btf_show *show, const char *fmt,
-			 va_list args)
+__printf(2, 0)
+static void btf_seq_show(struct btf_show *show, const char *fmt, va_list args)
 {
 	seq_vprintf((struct seq_file *)show->target, fmt, args);
 }
@@ -5173,6 +5173,7 @@ struct btf_show_snprintf {
 	int len;		/* length we would have written */
 };
 
+__printf(2, 0)
 static void btf_snprintf_show(struct btf_show *show, const char *fmt,
 			      va_list args)
 {
