1a7531404827785badd607ec24ad2de484136ac6
uwu-gl <moegluwu0@gmail.com>
2025-10-22 01:14:36 +0800

    net: ipv4: Remove unused tcp_pause_early_retransmit()
    Change-Id: I2e0b3d9a21f7d03a60d710472d45bad6827372b5
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -2505,29 +2505,6 @@ static inline int tcp_dupack_heuristics(const struct tcp_sock *tp)
 	return tcp_is_fack(tp) ? tp->fackets_out : tp->sacked_out + 1;
 }
 
-static bool tcp_pause_early_retransmit(struct sock *sk, int flag)
-{
-	struct tcp_sock *tp = tcp_sk(sk);
-	unsigned long delay;
-
-	/* Delay early retransmit and entering fast recovery for
-	 * max(RTT/4, 2msec) unless ack has ECE mark, no RTT samples
-	 * available, or RTO is scheduled to fire first.
-	 */
-	if (sysctl_tcp_early_retrans < 2 || sysctl_tcp_early_retrans > 3 ||
-	    (flag & FLAG_ECE) || !tp->srtt_us)
-		return false;
-
-	delay = max(usecs_to_jiffies(tp->srtt_us >> 5),
-		    msecs_to_jiffies(2));
-
-	if (!time_after(inet_csk(sk)->icsk_timeout, (jiffies + delay)))
-		return false;
-
-	inet_csk_reset_xmit_timer(sk, ICSK_TIME_EARLY_RETRANS, delay,
-				  TCP_RTO_MAX);
-	return true;
-}
 
 /* Linux NewReno/SACK/FACK/ECN state machine.
  * --------------------------------------
