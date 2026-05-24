9b72b3216c852c9af36bab4ad0aaa9ffa620fa97
krazey <admin@krazey.de>
2026-05-24 21:18:29 +0200

    fs: Keep names_cache dedicated

--- fs/dcache.c
+++ fs/dcache.c
@@ -3687,7 +3687,7 @@ void __init vfs_caches_init_early(void)
 void __init vfs_caches_init(void)
 {
 	names_cachep = kmem_cache_create("names_cache", PATH_MAX, 0,
-			SLAB_HWCACHE_ALIGN|SLAB_PANIC, NULL);
+			SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_NOLEAKTRACE, NULL);
 
 	dcache_init();
 	inode_init();
