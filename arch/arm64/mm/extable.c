13a38972d1878b48777d2347bf5b1654a5637ef0
Russell King <russell.king@oracle.com>
2026-08-03 08:39:37 +0200

    BACKPORT: arm64/bpf: Remove 128MB limit for BPF JIT programs
    commit b89ddf4cca43f1269093942cf5c4e457fd45c335 upstream.
    
    Commit 91fc957c9b1d ("arm64/bpf: don't allocate BPF JIT programs in module
    memory") restricts BPF JIT program allocation to a 128MB region to ensure
    BPF programs are still in branching range of each other. However this
    restriction should not apply to the aarch64 JIT, since BPF_JMP | BPF_CALL
    are implemented as a 64-bit move into a register and then a BLR instruction -
    which has the effect of being able to call anything without proximity
    limitation.
    
    The practical reason to relax this restriction on JIT memory is that 128MB of
    JIT memory can be quickly exhausted, especially where PAGE_SIZE is 64KB - one
    page is needed per program. In cases where seccomp filters are applied to
    multiple VMs on VM launch - such filters are classic BPF but converted to
    BPF - this can severely limit the number of VMs that can be launched. In a
    world where we support BPF JIT always on, turning off the JIT isn't always an
    option either.
    
    [ Mathias Gluszczynski: Keep __vmalloc_node_range() with
    PAGE_KERNEL_EXEC because this 4.9 tree does not make vmalloc() memory
    executable later. Search the BPF exception tables first because their
    ARM64 entries use a different fixup format.
    The 4.9 tree declares search_bpf_extables() in linux/module.h rather
    than linux/extable.h. Include the defining header before calling it
    from the ARM64 exception handler. ]
    
    Fixes: 91fc957c9b1d ("arm64/bpf: don't allocate BPF JIT programs in module memory")
    Suggested-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Russell King <russell.king@oracle.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Tested-by: Alan Maguire <alan.maguire@oracle.com>
    Link: https://lore.kernel.org/bpf/1636131046-5982-2-git-send-email-alan.maguire@oracle.com
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- arch/arm64/mm/extable.c
+++ arch/arm64/mm/extable.c
@@ -3,20 +3,24 @@
  */
 
 #include <linux/extable.h>
+#include <linux/module.h>
 #include <linux/uaccess.h>
 
 int fixup_exception(struct pt_regs *regs)
 {
 	const struct exception_table_entry *fixup;
+	unsigned long addr;
 
-	fixup = search_exception_tables(instruction_pointer(regs));
+	addr = instruction_pointer(regs);
+
+	/* Search the BPF tables first, these are formatted differently. */
+	fixup = search_bpf_extables(addr);
+	if (fixup)
+		return arm64_bpf_fixup_exception(fixup, regs);
+
+	fixup = search_exception_tables(addr);
 	if (!fixup)
 		return 0;
-
-	if (IS_ENABLED(CONFIG_BPF_JIT) &&
-	    regs->pc >= BPF_JIT_REGION_START &&
-	    regs->pc < BPF_JIT_REGION_END)
-		return arm64_bpf_fixup_exception(fixup, regs);
 
 	regs->pc = (unsigned long)&fixup->fixup + fixup->fixup;
 	return 1;
